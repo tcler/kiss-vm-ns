e6f7b51fe443bc5a90094ab0440cb6fe7b838de2
Jianhong Yin <jiyin@redhat.com>
2020-07-15 22:10:12 +0800

    examples/virtiofs-submount-test.sh: optimize code
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- examples/virtiofs-submount-test.sh
+++ examples/virtiofs-submount-test.sh
@@ -3,6 +3,7 @@
 sharedir=~/sharedir
 vmname=vm-virtiofs-submount
 distro=${1:-rhel-8.3%}
+user=$LOGNAME
 
 which vm &>/dev/null || {
 	echo -e "[WARN] you have not installed kiss-vm, please install kiss-vm first by run:"
@@ -22,13 +23,14 @@ mkfs.xfs ~/xfs2.img
 sudo mount -t ext4 ~/ext4.img $sharedir/ext4
 sudo mount -t xfs ~/xfs.img $sharedir/xfs
 sudo mount -t xfs ~/xfs2.img $sharedir/xfs2
+sudo chown -R $user $sharedir/{ext4,xfs,xfs2}
 
 vm $distro -n $vmname  --sharedir  $sharedir:hostshare -f
 
 vm exec $vmname -- mount -t virtiofs
 vm exec $vmname -- mountpoint /virtiofs/hostshare/xfs
 vm exec $vmname -- mountpoint /virtiofs/hostshare/xfs2
-sudo touch $sharedir/xfs/{1..16} $sharedir/xfs2/{1..16} $sharedir/testdir/{1..16}
+touch $sharedir/xfs/{1..16} $sharedir/xfs2/{1..16} $sharedir/testdir/{1..16}
 vm exec -v $vmname -- ls -li /virtiofs/hostshare/xfs | sort -n | head
 vm exec -v $vmname -- ls -li /virtiofs/hostshare/xfs2 | sort -n | head
 vm exec -v $vmname -- ls -li /virtiofs/hostshare/testdir | sort -n | head
