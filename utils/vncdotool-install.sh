1c8ebacd93c35e338cfbf7d84328980084fbafc5
Jianhong Yin <jiyin@redhat.com>
2020-08-28 17:31:51 +0800

    kiss-vm,utils/vncdotool-install.sh: --default-timeout=720 option
    avoid pip install fail with timeout
    
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/vncdotool-install.sh
+++ utils/vncdotool-install.sh
@@ -5,6 +5,6 @@ sudo yum install -y python-devel platform-python-devel python-pip python3-pip --
 PIP=pip
 which pip &>/dev/null || PIP=pip3
 
-sudo $PIP install --upgrade pip
-sudo $PIP install --upgrade setuptools
-sudo $PIP install vncdotool service_identity
+sudo $PIP --default-timeout=720 install --upgrade pip
+sudo $PIP --default-timeout=720 install --upgrade setuptools
+sudo $PIP --default-timeout=720 install vncdotool service_identity
