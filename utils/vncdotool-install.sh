03211d51a071cac318c744e430fac71391f8cee5
Jianhong Yin <jiyin@redhat.com>
2020-08-28 17:45:08 +0800

    kiss-vm,utils/vncdotool-install.sh: fix sudo pip fail
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/vncdotool-install.sh
+++ utils/vncdotool-install.sh
@@ -2,8 +2,8 @@
 
 #install packages required
 sudo yum install -y python-devel platform-python-devel python-pip python3-pip --setopt=strict=0
-PIP=pip
-which pip &>/dev/null || PIP=pip3
+PIP=$(which --skip-alias --skip functions pip 2>/dev/null)
+which pip &>/dev/null || PIP=$(which --skip-alias --skip functions pip3 2>/dev/null)
 
 sudo $PIP --default-timeout=720 install --upgrade pip
 sudo $PIP --default-timeout=720 install --upgrade setuptools
