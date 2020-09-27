7bd360ee5e1b97e5a91670b97d6810ec28b64042
Jianhong Yin <jiyin@redhat.com>
2020-09-27 12:47:37 +0800

    utils: add qcow2-connect.sh
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

---
+++ utils/qcow2-connect.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+qcow2img=$1
+
+freenbd() {
+	for x in /sys/class/block/nbd*; do
+		S=$(< $x/size)
+		[[ "$S" == "0" ]] && {
+			echo -n $x
+			break
+		}
+	done
+}
+
+# nbd module
+modprobe nbd max_part=64
+
+dev=$(freenbd)
+qemu-nbd --connect=$dev $qcow2img &&
+	echo $dev
+
+[[ "$DEBUG" = yes ]] && {
+	fdisk -l $dev
+	qemu-nbd --disconnect $dev
+}
