4866b3356f275d667c0a06f3aaf13cada46be51f
Jianhong Yin <jiyin@redhat.com>
2020-09-28 13:33:55 +0800

    utils/vglist.sh: improve to support auto rename duplicate vg names
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/vglist.sh
+++ utils/vglist.sh
@@ -1,6 +1,34 @@
 #!/bin/bash
 
-dev=$1
+Usage() {
+	echo "$0 [lvm2 device] [-n]"
+}
 
-#why there isn't vg_active field?
-pvs $dev -o vg_name,vg_uuid,lv_active --noheading|uniq
+vglist() {
+        local dev=("$@")
+        #why there isn't vg_active field?
+        pvs -o vg_name,vg_uuid,lv_active --noheading "${dev[@]}"|uniq
+}
+
+_at=`getopt -o hn \
+	--long help \
+    -a -n "$0" -- "$@"`
+eval set -- "$_at"
+while true; do
+	case "$1" in
+	-h|--help) Usage; shift 1; exit 0;;
+	-n)        RenameDuplicateName=no; shift 1;;
+	--) shift; break;;
+	esac
+done
+
+#rename vgname if there are volume groups with same names
+[[ "$RenameDuplicateName" != no ]] && {
+	while read head vgs; do
+		for vg in $vgs; do
+			vgrename -v $vg ${head}-$((++i))
+		done
+	done < <(vglist | awk '!/active$/ {a[$1]++; b[$1]=b[$1] FS $2; } END {for(vg in a) if (a[vg]>1) print vg, b[vg]}')
+}
+
+vglist "$@"
