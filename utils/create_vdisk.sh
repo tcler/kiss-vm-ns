a4bcc577490a38b98614c4f47c01401ec0e22294
Jianhong Yin <jiyin@redhat.com>
2020-11-19 13:37:29 +0800

    create_vdisk.sh: add partprobe after fdisk
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/create_vdisk.sh
+++ utils/create_vdisk.sh
@@ -8,6 +8,7 @@ create_vdisk() {
 	dd if=/dev/null of=$path bs=1${size//[0-9]/} seek=${size//[^0-9]/}
 	local dev=$(losetup --partscan --show --find $path)
 	printf "o\nn\np\n1\n\n\nw\n" | fdisk "$dev"
+	partprobe "$dev"
 	mkfs.$fstype $MKFS_OPT "${dev}p1"
 	losetup -d $dev
 }
