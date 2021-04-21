1ff8cb9d9a24a9b7aed87ed50f3301e1fa8bfe7a
Jianhong Yin <jiyin@redhat.com>
2021-04-21 12:15:00 +0800

    examples/rh-training-lab.sh: optimize code and fix wrong route setup
    Supported-by: Jianlin Shi <jishi@redhat.com>
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- examples/rh-training-lab.sh
+++ examples/rh-training-lab.sh
@@ -50,27 +50,44 @@ tmux new -d "/usr/bin/vm create -f $distro -i $imagef --nointeract --net classro
 tmux new -d "/usr/bin/vm create -f $distro -i $imagef --nointeract --net classroom --net-macvtap -n classroom"
 
 port_available() { nc $1 $2 </dev/null &>/dev/null; }
-for vm in $vmlist; do
-	echo -e "\n{INFO} waiting VM($vm) install finish ..."
-	until port_available $vm 22; do sleep 2; done
+for _vm in $vmlist; do
+	echo -e "\n{INFO} waiting VM($_vm) install finish ..."
+	until port_available $_vm 22; do sleep 2; done
 done
 
 serveraAddr=$(vm if servera)
 classroomAddr=$(vm if classroom)
-vm exec -v bastion -- sysctl -w net.ipv4.ip_forward=1
-vm exec -v servera -- ip route add 172.25.252.0/24 via $serveraAddr
-vm exec -v classroom -- ip route add 172.25.250.0/24 via $classroomAddr
+bastionIpAddrs=$(vm exec bastion -- ip a)
+read bastionAddr252 _ < <(echo "$bastionIpAddrs" | awk -F'[ /]+' '/ *inet 172.25.252/{printf $3 " "}')
+read bastionAddr250 _ < <(echo "$bastionIpAddrs" | awk -F'[ /]+' '/ *inet 172.25.250/{printf $3 " "}')
 
-vm exec -v bastion -- sysctl net.ipv4.ip_forward=1
+#check default setup of net.ipv4.ip_forward and net.ipv4.conf.all.forwarding
+vm exec -v bastion -- sysctl net.ipv4.ip_forward \#check default ipv4.ip_forward setup
+vm exec -v bastion -- sysctl net.ipv4.conf.all.forwarding \#check default ipv4.conf.all.forwarding setup
+
+#enable ip forward on bastion
+#vm exec -v bastion -- sysctl -w net.ipv4.conf.all.forwarding=1
+vm exec -v bastion -- sysctl -w net.ipv4.ip_forward=1
+
+#setup route table
+vm exec -v classroom -- ip route add 172.25.250.0/24 via $bastionAddr252
+_vmlist="workstation servera serverb utility"
+for _vm in $_vmlist; do
+	vm exec -v $_vm -- ip route add 172.25.252.0/24 via $bastionAddr250
+done
+
+#check route table
 vm exec -v servera -- ip route s
 vm exec -v classroom -- ip route s
 
-read bastionAddr1 bastionAddr2 _ < <(vm exec bastion -- ip a | awk -F'[ /]+' '/ *inet 172/{printf $3 " "}')
-vm exec -v servera -- ping -c 2 $bastionAddr1   \#ping router bastion addr1
-vm exec -v servera -- ping -c 2 $bastionAddr2   \#ping router bastion addr2
-vm exec -v classroom -- ping -c 2 $bastionAddr1 \#ping router bastion addr1
-vm exec -v classroom -- ping -c 2 $bastionAddr2 \#ping router bastion addr2
+#ping test
+vm exec -v bastion -- sysctl net.ipv4.ip_forward \#check ipv4.ip_forward setup
+vm exec -v bastion -- sysctl net.ipv4.conf.all.forwarding \#check ipv4.conf.all.forwarding setup
+vm exec -v servera -- ping -c 2 $bastionAddr252   \#ping router bastion addr1
+vm exec -v servera -- ping -c 2 $bastionAddr250   \#ping router bastion addr2
+vm exec -v classroom -- ping -c 2 $bastionAddr252 \#ping router bastion addr1
+vm exec -v classroom -- ping -c 2 $bastionAddr250 \#ping router bastion addr2
+vm exec -v servera -- ping -c 3 $classroomAddr  \#ping classroom
+vm exec -v classroom -- ping -c 3 $serveraAddr  \#ping servera
 
-vm exec -v bastion -- sysctl net.ipv4.ip_forward \#confirm ip_forward is enabled
-vm exec -v servera -- ping -c 4 $classroomAddr  \#ping classroom
-vm exec -v classroom -- ping -c 4 $serveraAddr  \#ping servera
+#TBD: ...
