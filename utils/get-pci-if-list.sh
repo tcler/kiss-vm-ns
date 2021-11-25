29b130d12226104a58d385b43eceafb2a5d10a4c
Jianhong Yin <jiyin@redhat.com>
2021-11-25 19:39:53 +0800

    utils: add get-pci-if-list.sh
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

---
+++ utils/get-pci-if-list.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+get_pci_if_list() {
+	for ifdir in /sys/class/net/*; do
+		ueventf=$ifdir/device/uevent
+		if [[ -e $ueventf ]]; then
+			pciAddr=$(awk -F= '/PCI_SLOT_NAME/{print $2}' $ueventf)
+			echo -e "${pciAddr}\t${ifdir##*/}"
+		fi
+	done
+}
+
+get_pci_if_list
