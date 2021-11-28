1bef20f3f3611f140f8a6982c97a30945c5fb7f1
Jianhong Yin <jiyin@redhat.com>
2021-11-28 08:13:40 +0800

    utils/clear-vms-home: cover more scenarios
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/clear-vms-home.sh
+++ utils/clear-vms-home.sh
@@ -5,6 +5,8 @@ for vmdir in $VMSHOME/*/*; do
 	vmname=${vmdir##*/}
 	if ! virsh desc $vmname &>/dev/null; then
 		rm -rf $vmdir
+	elif ! virsh dumpxml --domain $vmname|grep -q $vmdir; then
+		rm -rf $vmdir
 	fi
 done
 rmdir $VMSHOME/* 2>/dev/null
