1ed5052ebd6b910602bb5f02ac4d8707542e6b5f
Jianhong Yin <jiyin@redhat.com>
2022-01-12 19:48:17 +0800

    utils/iommu-groups.sh: order by group id
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/iommu-groups.sh
+++ utils/iommu-groups.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+iommuGrpsRoot=/sys/kernel/iommu_groups
+
 shopt -s nullglob
-for grp in /sys/kernel/iommu_groups/*; do
-	echo "IOMMU Group ${grp##*/}:"
-	for dev in $grp/devices/*; do
-		echo "    $(lspci -nns ${dev##*/})"
+for grp in $(ls $iommuGrpsRoot | sort -V); do
+	echo "IOMMU Group ${grp}:"
+	for dev in $(ls $iommuGrpsRoot/$grp/devices); do
+		echo "    $(lspci -nns ${dev})"
 	done
 done
