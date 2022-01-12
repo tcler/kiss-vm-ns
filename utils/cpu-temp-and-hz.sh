ec334d8d82e66c4e8a92849cfb6916f6eb52c94b
Jianhong Yin <jiyin@redhat.com>
2022-01-12 15:45:03 +0800

    utils/cpu-temp-and-hz.sh: optimize code
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/cpu-temp-and-hz.sh
+++ utils/cpu-temp-and-hz.sh
@@ -3,10 +3,10 @@
 watch -t -n 1 '
 	date
 	echo
-	if command -v sensors &>/dev/null; then
-		sensors|sed -n "/Physical id 0/{s/[^+]*+//; p;}"
-	else
+	if test -f /sys/class/thermal/thermal_zone0/temp; then
 		echo CPU: $(($(cat /sys/class/thermal/thermal_zone0/temp 2>/dev/null||echo 0)/1000))°
+	else
+		sensors|sed -n "/Physical id 0/{s/[^+]*+//; p;}"
 	fi
 	echo
 	grep cpu.MHz /proc/cpuinfo | sort'
