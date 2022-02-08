1112a14a9db90b1484a68bc6c4ed52c27cd53366
Jianhong Yin <jiyin@redhat.com>
2022-02-08 17:26:16 +0800

    utils/disable-ipv6: fix error on slackware
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/disable-ipv6.sh
+++ utils/disable-ipv6.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
+if command -v sysctl; then
+	sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
+else
+	sudo bash -c 'echo 1 >/proc/sys/net/ipv6/conf/all/disable_ipv6'
+fi
