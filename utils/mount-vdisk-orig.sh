0d77e48d58001cce17bdca6898ecf6a71ea3195f
Jianhong Yin <jiyin@redhat.com>
2022-03-10 19:55:48 +0800

    utils: rename mount-vdisk*.sh
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

---
+++ utils/mount-vdisk-orig.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+LANG=C
+PROG=${0##*/}
+
+mount_vdisk() {
+	local path=$1
+	local mp=$2
+	local partN=${3:-1}
+	local offset=
+
+	if fdisk -l -o Start "$path" &>/dev/null; then
+		read offset sizelimit < <(fdisk -l -o Start,Sectors "$path" |
+			awk -v N=$partN '
+				/^Units:/ { unit=$(NF-1); offset=0; }
+				/^Start/ {
+					for(i=0;i<N;i++)
+						if(getline == 0) { $0=""; break; }
+					offset=$1*unit;
+					sizelimit=$2*unit;
+				}
+				END { print offset, sizelimit; }'
+		)
+	else
+		read offset sizelimit < <(fdisk -l "$path" |
+			awk -v N=$partN '
+				/^Units/ { unit=$(NF-1); offset=0; }
+				$3 == "Start" {
+					for(i=0;i<N;i++)
+						if(getline == 0) { $0=""; break; }
+					offset=$2*unit; sizelimit=($3-$2)*unit;
+					if ($2 == "*") {
+						offset=$3*unit; end=($4-$3)*unit;
+					}
+				}
+				END { print offset, sizelimit; }'
+		)
+	fi
+	echo "offset: $offset, sizelimit: $sizelimit"
+
+	[[ -d "$mp" ]] || {
+		echo "{warn} mount_vdisk: dir '$mp' not exist"
+		return 1
+	}
+
+	if [[ "$offset" -ne 0 || "$partN" -eq 1 ]]; then
+		mount $MNT_OPT -oloop,offset=$offset,sizelimit=$sizelimit $path $mp
+	else
+		echo "{warn} mount_vdisk: there's not part($partN) on disk $path"
+		return 1
+	fi
+}
+
+[[ $# -lt 2 ]] && {
+	cat <<-COMM
+	Usage: [MNT_OPT=xxx] sudo $0 <image> <mountpoint> [partition Number]
+
+	Examples:
+	  $0 usb.img /mnt/usb
+	  $0 ext4.img /mnt/ext4
+	  MNT_OPT="-oro" $0 xfs.img /mnt/xfstest 2
+	COMM
+	exit 1
+}
+test $(id -u) = 0 || {
+	echo "{Warn} ${PROG} need root permission, try: sudo ${PROG} ..." >&2
+	exit 1
+}
+
+mount_vdisk "$@"
--- utils/mount-vdisk.sh
+++ utils/mount-vdisk.sh
@@ -3,68 +3,47 @@
 LANG=C
 PROG=${0##*/}
 
-mount_vdisk() {
+mount_vdisk2() {
 	local path=$1
-	local mp=$2
-	local partN=${3:-1}
-	local offset=
+	local partN=${2:-1}
+	local dev= mntdev= mntopt= mntinfo=
 
-	if fdisk -l -o Start "$path" &>/dev/null; then
-		read offset sizelimit < <(fdisk -l -o Start,Sectors "$path" |
-			awk -v N=$partN '
-				/^Units:/ { unit=$(NF-1); offset=0; }
-				/^Start/ {
-					for(i=0;i<N;i++)
-						if(getline == 0) { $0=""; break; }
-					offset=$1*unit;
-					sizelimit=$2*unit;
-				}
-				END { print offset, sizelimit; }'
-		)
-	else
-		read offset sizelimit < <(fdisk -l "$path" |
-			awk -v N=$partN '
-				/^Units/ { unit=$(NF-1); offset=0; }
-				$3 == "Start" {
-					for(i=0;i<N;i++)
-						if(getline == 0) { $0=""; break; }
-					offset=$2*unit; sizelimit=($3-$2)*unit;
-					if ($2 == "*") {
-						offset=$3*unit; end=($4-$3)*unit;
-					}
-				}
-				END { print offset, sizelimit; }'
-		)
+	read dev _ < <(losetup -j $path|awk -F'[: ]+' '{print $1, $2}')
+	if [[ -z "$dev" ]]; then
+		udisksctl loop-setup -f $path >&2
+		read dev _ < <(losetup -j $path|awk -F'[: ]+' '{print $1, $2}')
 	fi
-	echo "offset: $offset, sizelimit: $sizelimit"
-
-	[[ -d "$mp" ]] || {
-		echo "{warn} mount_vdisk: dir '$mp' not exist"
+	[[ -z "$dev" ]] && {
+		echo "{err} 'losetup -j $path' got fail, I don't know why" >&2
 		return 1
 	}
 
-	if [[ "$offset" -ne 0 || "$partN" -eq 1 ]]; then
-		mount $MNT_OPT -oloop,offset=$offset,sizelimit=$sizelimit $path $mp
+	mntdev=${dev}p${partN}
+	{ ls -l ${mntdev}; } >&2
+
+	mntinfo=$(mount | awk -v d=$mntdev '$1 == d')
+	loinfo=$(losetup -j $path)
+	if [[ -z "$mntinfo" ]]; then
+		mntopt=$([[ -n "$MNT_OPT" ]] && echo --options=$MNT_OPT)
+		udisksctl mount -b $mntdev $mntopt >&2
 	else
-		echo "{warn} mount_vdisk: there's not part($partN) on disk $path"
-		return 1
+		echo -e "{warn} '$path' has been already mounted:\n  $mntinfo" >&2
 	fi
+
+	echo -e "  $loinfo" >&2
+	mount | awk -v d=$mntdev '$1 == d {print $3}'
 }
 
-[[ $# -lt 2 ]] && {
+[[ $# -lt 1 ]] && {
 	cat <<-COMM
-	Usage: [MNT_OPT=xxx] sudo $0 <image> <mountpoint> [partition Number]
+	Usage: [MNT_OPT=xxx] $0 <image> [partition Number]
 
 	Examples:
-	  $0 usb.img /mnt/usb
-	  $0 ext4.img /mnt/ext4
-	  MNT_OPT="-oro" $0 xfs.img /mnt/xfstest 2
+	  $0 usb.img
+	  $0 ext4.img 1
+	  MNT_OPT="-oro" $0 xfs.img 2
 	COMM
 	exit 1
 }
-test $(id -u) = 0 || {
-	echo "{Warn} ${PROG} need root permission, try: sudo ${PROG} ..." >&2
-	exit 1
-}
 
-mount_vdisk "$@"
+mount_vdisk2 "$@"
