222d520cd2c80393b2250c20e4d9a065f3b09c68
Jianhong Yin <jiyin@redhat.com>
2022-03-12 08:10:39 +0800

    utils/create-vdisk.sh: optimize code
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/create-vdisk.sh
+++ utils/create-vdisk.sh
@@ -2,38 +2,6 @@
 
 LANG=C
 
-dd_range() {
-	local if=$1
-	local of=$2
-	local SKIP=${3:-0}
-	local SIZE=$4
-	local BS=$((8 * 1024))
-	local Q= R= Q2= R2= NREAD=
-
-	[[ -z "$of" ]] && return 1
-	local fsize=$(stat -c%s "$if") || return $?
-
-	((fsize <= BS)) && BS=$SKIP
-	Q=$((SKIP/BS))  #quotient
-	R=$((SKIP%BS))  #residue
-	NREAD=$((BS-R))
-	dd if=$if ibs=$BS skip=$Q count=1 | tail -c $NREAD >$of
-
-	if [[ -z "$SIZE" ]]; then
-		dd if=$if bs=$BS skip=$((Q+1)) oflag=append conv=notrunc of=$of
-	else
-		if ((NREAD > SIZE)); then
-			truncate --size=${SIZE} $of
-			return 0
-		fi
-		let SIZE-=$NREAD
-		Q2=$((SIZE/BS))  #quotient
-		R2=$((SIZE%BS))  #residue
-		((Q2>0)) && dd if=$if bs=$BS skip=$((Q+1)) count=$Q2 oflag=append conv=notrunc of=$of
-		((R2>0)) && dd if=$if bs=$BS skip=$((Q+1+Q2)) count=1 | head -c $R2 >>$of
-	fi
-}
-
 create_vdiskn() {
 	local path=$1
 	local dsize=$2
@@ -51,7 +19,7 @@ create_vdiskn() {
 		awk -v P=1 '/^Number/{start=1;next}; start {if ($1==P) {print $2, $4}}' )
 	echo -e "\n[$fn:info] split disk head and partition($pstart:$psize)"
 	dd if=$path of=$imghead bs=${pstart} count=1
-	dd_range $path $imgtail $((pstart)) ${psize}
+	truncate --size=${psize} $imgtail
 
 	echo -e "\n[$fn:info] making fs($fstype)"
 	mkfs.$fstype $MKFS_OPT "$imgtail"
