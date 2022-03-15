6de4550f0ae99794a4fd21b470993af35cd24636
Jianhong Yin <jiyin@redhat.com>
2022-03-15 11:40:20 +0800

    utils/dd-file-range.sh: optimize code
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/dd-file-range.sh
+++ utils/dd-file-range.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
+#
+#this program is used to copy part of src file into somewhere in dst file
+#like syscall copy_file_range(2) on linux kernel-5.3
+#
+#you could also use the xfs_io -> 'copy_range' sub-command in newer linux
+#distributions instead
 
 dd_file_range_old() {
+	maxInt() { bc <<<"$(printf %u -1)/2"; }
+
 	#assume dd has not supported skip_bytes,seek_bytes flag
 	local if=$1
 	local of=$2
 	local skip=${3:-0}
 	local seek=${4:-0}
-	local len=$5
+	local len=${5:-$(maxInt)}
 	local BS=$((16*1024))
 	local fn=${FUNCNAME[0]}
 	local logOpt=${LogOpt:-status=none}
@@ -23,7 +31,6 @@ dd_file_range_old() {
 	}
 	[[ -z "$of" ]] && return 1
 	touch "$of" || return $?
-	local ofsize=$(stat -c%s "$of") || return $?
 
 	local tmpof=$(mktemp)
 	local Q= R= Q2= R2= NREAD=
@@ -35,22 +42,18 @@ dd_file_range_old() {
 	NREAD=$((iBS-R))
 	dd if="$if" ibs=$iBS skip=$Q count=1 $logOpt | tail -c $NREAD >$tmpof
 
-	if [[ -z "$len" ]]; then
-		((alen > NREAD)) &&
-			dd if="$if" ibs=$iBS skip=$((Q+1)) oflag=append conv=notrunc of="$tmpof" $logOpt
-	else
-		if ((len > NREAD)); then
-			let len-=$NREAD
-			Q2=$((len/iBS))  #quotient
-			R2=$((len%iBS))  #residue
-			((Q2>0)) && dd if="$if" ibs=$iBS skip=$((Q+1)) count=$Q2 oflag=append conv=notrunc of="$tmpof" $logOpt
-			((R2>0)) && dd if="$if" ibs=$iBS skip=$((Q+1+Q2)) count=1 $logOpt | head -c $R2 >>"$tmpof"
-		else
-			truncate --size=${len} "$tmpof"
-		fi
+	if ((len > NREAD)); then
+		let len-=$NREAD
+		Q2=$((len/iBS))  #quotient
+		R2=$((len%iBS))  #residue
+		((Q2>0)) && dd if="$if" ibs=$iBS skip=$((Q+1)) count=$Q2 oflag=append conv=notrunc of="$tmpof" $logOpt
+		((R2>0)) && dd if="$if" ibs=$iBS skip=$((Q+1+Q2)) count=1 $logOpt | head -c $R2 >>"$tmpof"
+	elif ((len < NREAD)); then
+		truncate --size=${len} "$tmpof"
 	fi
 
 	if ((seek > 0)); then
+		local ofsize=$(stat -c%s "$of")
 		((seek > ofsize)) && dd if=/dev/zero bs=1c seek=$seek count=0 of="$of" $logOpt
 		Q=$((seek/BS))
 		R=$((seek%BS))
@@ -102,10 +105,8 @@ dd_file_range() {
 args=()
 for arg; do
 	case "$arg" in
-	-skip=*) _skip=${arg/*=/};;
-	-seek=*) _seek=${arg/*=/};;
-	-len=*)  _len=${arg/*=/};;
-	-log=*)  _logLevel=${arg/*=/};;
+	-s*=*)   SEP=${arg/*=/};;
+	-log=*)  LogLevel=${arg/*=/};;
 	-ver=*)  _ver=${arg/*=/};;
 	-*)      :;;
 	*)       args+=($arg);;
@@ -114,33 +115,36 @@ done
 eval set -- "${args[@]}"
 [[ $# -lt 2 ]] && {
 	cat <<-COMM
-	Usage: $0 <ifile> <ofile> [skip [seek [len]]] [[-skip=|-seek=|-len=|-log=<noxfer|progress>]...]
+	Usage: $0 <ifile[:offset[:len]]> <ofile[:offset]> [-sep=<seperator>] [-log=<0|1|2>]
 
 	Examples:
-	  $0 ifile ofile 4096 -len=\$((64*1024))
-	  $0 ifile ofile -seek=\$((8*1024))
+	  $0 ifile:8192:\$((32*1024))  ofile
+	  $0 ifile::4096              ofile:\$((8*1024))
 
 	Tests:
 	  echo -n "0123456789abcdef" >a; echo -n "^*******************************" >b
-	  $0 a b 3 8  4;  sed "s/$/\n/" b
-	  $0 a b 3 8  6;  sed "s/$/\n/" b
-	  $0 a b 3 8  8;  sed "s/$/\n/" b
-	  $0 a b 3 8  10; sed "s/$/\n/" b
-	  $0 a b 3 8  12; sed "s/$/\n/" b
-	  $0 a b 3 8  14; sed "s/$/\n/" b
-	  $0 a b 3 8  16; sed "s/$/\n/" b
-	  $0 a b 3 64 13; sed "s/$/\n/" b
+	  $0 a::4   b:8;  sed "s/$/\n/" b
+	  $0 a:3:6  b:8;  sed "s/$/\n/" b
+	  $0 a:3:8  b:8;  sed "s/$/\n/" b
+	  $0 a:3:10 b:8;  sed "s/$/\n/" b
+	  $0 a:3:12 b:8;  sed "s/$/\n/" b
+	  $0 a:3:14 b:8;  sed "s/$/\n/" b
+	  $0 a:3:16 b:8;  sed "s/$/\n/" b
+	  $0 a:3:16 b:64; sed "s/$/\n/" b
 	  rm -f -- a b
 	COMM
 	exit 1
 }
 
-if=$1; of=$2; shift 2;
-read skip seek len <<<"$*"
-skip=${skip:-${_skip:-0}}
-seek=${seek:-${_seek:-0}}
-len=${len:-${_len}}
-LogOpt=status=${_logLevel:-none}
+SEP=${SEP:-:}
+IFS=$SEP read if skip len <<<"${1}"
+IFS=$SEP read of seek <<<"${2}"
+skip=${skip:-0}
+seek=${seek:-0}
+status=none
+case "${LogLevel}" in (1) status=noxfer;; (2) status=progress;; esac
+logOpt=status=$status
+
 if [[ -n "$_ver" ]]; then
 	case "$_ver" in
 	o*) dd_file_range_old "$if" "$of" $skip $seek $len;;
