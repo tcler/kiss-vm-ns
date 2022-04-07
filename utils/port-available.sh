878f433f80337d9e25472132784f6df79f2313d6
Jianhong Yin <jiyin@redhat.com>
2022-04-07 09:24:42 +0800

    utils/port-available.sh: fix unexpected result on RHEL-7
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/port-available.sh
+++ utils/port-available.sh
@@ -13,7 +13,7 @@ port_available() {
 	if grep -q -- '-z\>' < <(nc -h 2>&1); then
 		nc -z $1 $2 </dev/null &>/dev/null
 	elif command -v nmap >/dev/null; then
-		nmap $1 -p $2 | grep -q open
+		nmap -Pn $1 -p $2 | grep -q open
 	else
 		timeout 0.1 curl -s -v telnet://$1:$2 |& grep -q ^..Connected
 	fi
