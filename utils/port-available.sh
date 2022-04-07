49288e3774f57cec75ec316a553bcb8e3f83de80
Jianhong Yin <jiyin@redhat.com>
2022-04-07 08:21:19 +0800

    utils/port-available.sh: add optional curl method
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/port-available.sh
+++ utils/port-available.sh
@@ -3,8 +3,10 @@
 port_available() {
 	if grep -q -- '-z\>' < <(nc -h 2>&1); then
 		nc -z $1 $2 </dev/null &>/dev/null
-	else
+	elif command -v nmap >/dev/null; then
 		nmap $1 -p $2 | grep -q open
+	else
+		timeout 0.1 curl -s -v telnet://$1:$2 |& grep -q ^..Connected
 	fi
 }
 
