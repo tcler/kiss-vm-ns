3eaeefd4d6eda0e65018e8f390b7828a78bd2876
Jianhong Yin <jiyin@redhat.com>
2022-04-07 08:06:44 +0800

    utils/port-available.sh: fix if nc command not support -z option
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/port-available.sh
+++ utils/port-available.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 port_available() {
-        nc $(grep -q -- '-z\>' < <(nc -h 2>&1) && echo -z) $1 $2 </dev/null &>/dev/null
+	if grep -q -- '-z\>' < <(nc -h 2>&1); then
+		nc -z $1 $2 </dev/null &>/dev/null
+	else
+		nmap $1 -p $2 | grep -q open
+	fi
 }
 
 port_available "$@"
