4bd30c8bc329da07ac84fcf45407463d7e00cf62
Jianhong Yin <jiyin@redhat.com>
2022-04-07 09:08:09 +0800

    utils/port-available.sh: add -w[=sencond] option
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/port-available.sh
+++ utils/port-available.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+_at=()
+for arg; do
+	case "$arg" in
+	(--w*|-w*) WAIT=yes; [[ "$arg" = *=* ]] && TIME=${arg/*=/};;
+	(*) _at+=($arg);;
+	esac
+	shift
+done
+
 port_available() {
 	if grep -q -- '-z\>' < <(nc -h 2>&1); then
 		nc -z $1 $2 </dev/null &>/dev/null
@@ -10,4 +19,19 @@ port_available() {
 	fi
 }
 
-port_available "$@"
+rc=1
+if [[ "$WAIT" != yes ]]; then
+	port_available "${_at[@]}"; rc=$?
+else
+	TIME=${TIME//[^0-9]/}
+	TIME=${TIME:-forever}
+	CNT=$(((TIME+10)/10))
+	echo "[INFO] waiting port ${_at[@]} available, max time(${TIME})"
+	for ((i=0; i<CNT; i++)); do
+		port_available "${_at[@]}"; rc=$?
+		[[ $rc = 0 ]] && break
+		[[ "$TIME" = forever ]] && i=0
+		sleep 10
+	done
+fi
+exit $rc
