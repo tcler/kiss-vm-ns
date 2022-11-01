3f82bfc71993c49cd66b740d04f76526d99eb0b6
Jianhong Yin <jiyin@redhat.com>
2022-11-01 17:34:13 +0800

    utils: add apue-3e-get.sh
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

---
+++ utils/apue-3e-get.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#download apue-3e example,lib code and install apue lib and header files
+#
+
+switchroot() {
+	local P=$0; [[ $0 = /* ]] && P=${0##*/}
+	[[ $(id -u) != 0 ]] && {
+		echo -e "{WARN} $P need root permission, switch to:\n  sudo $P $@" | GREP_COLORS='ms=1;30' grep --color=always . >&2
+		exec sudo $P "$@"
+	}
+}
+switchroot "$@"
+
+tarf=src.3e.tar.gz
+dir=apue.3e
+
+pushd /usr/src
+	wget http://www.apuebook.com/$tarf -O $tarf
+	tar zxf $tarf
+	make -C $dir/lib &&
+		cp $dir/lib/libapue.a /usr/lib/. &&
+		cp $dir/include/apue.h /usr/include/.
+	make -C $dir/lib clean
+
+	echo
+	readlink -f $dir
+	ls -l $dir
+popd >/dev/null
