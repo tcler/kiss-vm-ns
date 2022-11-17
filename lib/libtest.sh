df59e6d49a49b61f4e24973c3796e70b2635c7f5
Jianhong Yin <jiyin@redhat.com>
2022-11-17 11:00:27 +0800

    lib/libtest.sh: getReusableCommandLine() rename _EVAL to _LAZY
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- lib/libtest.sh
+++ lib/libtest.sh
@@ -51,7 +51,7 @@ chkrc() {
 	return $_RC
 }
 
-_EVAL=no
+_LAZY=no
 getReusableCommandLine() {
 	#if only one parameter, treat it as a piece of script
 	[[ $# = 1 ]] && { echo "$1"; return; }
@@ -59,7 +59,7 @@ getReusableCommandLine() {
 	local shpattern='^[][0-9a-zA-Z~@%^_+=:,./-]+$'
 
 	for at; do
-		if [[ "$_EVAL" = yes ]]; then
+		if [[ "$_LAZY" = yes ]]; then
 			case "$at" in
 			\||\;|\>*|\&\>*|[0-9]\>*|\<|\<*)
 				echo -n "$at "
@@ -144,7 +144,7 @@ run() {
 	local _cmdl=$(getReusableCommandLine "$@")
 	local _cmdlx=
 	[[ $# -ne 1 && "$_runtype" = eval ]] &&
-		_cmdl=$(_EVAL=yes getReusableCommandLine "$@")
+		_cmdl=$(_LAZY=yes getReusableCommandLine "$@")
 
 	if [[ "$_debug" = yes ]]; then
 		if [[ "${_runtype}" = tmux ]]; then
