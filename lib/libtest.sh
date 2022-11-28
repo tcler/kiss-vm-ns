29855eafcf815a7bacdee6dadc3de2dd6ce212b8
Jianhong Yin <jiyin@redhat.com>
2022-11-28 12:19:29 +0800

    lib/libtest.sh,utils: mv support-virtualization() to utils as a
    script
    
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- lib/libtest.sh
+++ lib/libtest.sh
@@ -14,16 +14,6 @@ switchroot() {
 	}
 }
 
-support_virtualization() {
-	local rc=1
-	case ${OSTYPE,,} in
-	linux-gnu*) egrep -q -wo '(vmx|svm)' /proc/cpuinfo && rc=0;;
-	freebsd)    dmesg | grep -q -e VT-x: -e SVM: && rc=0;;
-	*)          echo "{WARN} unsupported OS/platform." >&2;;
-	esac
-	return $rc
-}
-
 _range2list() {
 	local range=$1  #like: 1,3,5,11-100,127
 	local list=()
---
+++ utils/support-virtualization.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+support_virtualization() {
+	local rc=1
+	case ${OSTYPE,,} in
+	linux-gnu*) egrep -q -wo '(vmx|svm)' /proc/cpuinfo && rc=0;;
+	freebsd)    dmesg | grep -q -e VT-x: -e SVM: && rc=0;;
+	*)          echo "{WARN} unsupported OS/platform." >&2;;
+	esac
+	return $rc
+}
+
+support_virtualization
