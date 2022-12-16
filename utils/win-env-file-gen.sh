c10900e75fcc6fb9d3fc3a50c27ed5a71ab6851b
Jianhong Yin <jiyin@redhat.com>
2022-12-16 16:16:32 +0800

    win-env-file-gen.sh: change default permission of env file
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/win-env-file-gen.sh
+++ utils/win-env-file-gen.sh
@@ -24,7 +24,7 @@ echo "[win-envf:INFO] generating windows env file: $WIN_ENV_FILE"
 
 # Get install and ipconfig log
 WIN_DATA=/tmp/$vmname-data
-rm -fr $WIN_DATA && mkdir -p $WIN_DATA
+rm -fr $WIN_DATA && mkdir -p $WIN_DATA && chmod a+rwx $WIN_DATA
 if ! vm homedir $vmname|egrep -iq '(win|windows)-?7'; then
 	vm cpfrom $vmname C:/postinstall_logs/* $WIN_DATA
 	iconv -f UTF-16LE -t UTF-8 $WIN_DATA/postinstall.log -o $WIN_DATA/postinstall.log
@@ -33,6 +33,7 @@ else
 	vm exec $vmname cat C:/win.env </dev/null >$WIN_DATA/win.env
 fi
 dos2unixf $WIN_DATA/*
+chmod a+rw $WIN_DATA/*
 
 # Save relative variables into a log file
 VM_INT_IP=$(awk '/^ *IPv4 Address/ {if ($NF ~ /^192/) print $NF}' $WIN_DATA/ipconfig.log)
@@ -40,9 +41,11 @@ VM_EXT_IP=$(awk '/^ *IPv4 Address/ {if ($NF !~ /^(192|169.254)/) print $NF}' $WI
 VM_EXT_IP6=$(awk '/^ *IPv6 Address/ {printf("%s,", $NF)}'  $WIN_DATA/ipconfig.log)
 [[ -z "$VM_EXT_IP" ]] && VM_EXT_IP=${VM_EXT_IP6%%,*}
 
+rm -f $WIN_ENV_FILE
 cat $WIN_DATA/win.env - <<-EOF | grep -v '^#' | tee $WIN_ENV_FILE
 
 	VM_INT_IP=$VM_INT_IP
 	VM_EXT_IP=$VM_EXT_IP
 	VM_EXT_IP6=$VM_EXT_IP6
 EOF
+chmod a+rw $WIN_ENV_FILE
