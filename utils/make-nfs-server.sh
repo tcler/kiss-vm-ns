8f13240509c2a6360e5918e5361a2b2fb35b4a1a
Jianhong Yin <jiyin@redhat.com>
2022-12-22 14:11:36 +0800

    utils/make-nfs-server.sh: add -t option and optimize code
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/make-nfs-server.sh
+++ utils/make-nfs-server.sh
@@ -18,18 +18,21 @@ Usage:
 Options:
   -h, -help              ; show this help
   -prefix <path>         ; root directory of nfs share(default: /nfsshare/)
+  -t                     ; run extra tests after nfs start
 EOF
 }
 test `id -u` = 0 || { echo "{Warn} This command has to be run under the root user"|grep --color=always . >&2; Usage >&2; exit 1; }
 
-_at=$(getopt -a -o h \
+_at=$(getopt -a -o ht \
 	--long help \
+	--long test \
 	--long prefix: \
 	-n "$P" -- "$@")
 eval set -- "$_at"
 while true; do
 	case "$1" in
 	-h|--help)    Usage; shift 1; exit 0;;
+	-t|--test)    eTEST=yes; shift 1;;
 	--prefix)     PREFIX=$2; shift 2;;
 	--) shift; break;;
 	esac
@@ -37,7 +40,7 @@ done
 
 
 ## install related packages
-yum install -y nfs-utils &>/dev/null
+rpm -q nfs-utils || yum install -y nfs-utils &>/dev/null
 #yum install -y krb5-workstation &>/dev/null
 
 
@@ -64,7 +67,10 @@ systemctl enable nfs-server
 systemctl restart nfs-server
 
 ## test/verify
+echo $'\n\E[0;33;44m'"{Info} showmount -e localhost"$'\E[0m'
 showmount -e localhost
+
+[[ "$eTEST" != yes ]] && exit
 
 ## one more test about nfsv4 pseudo-filesystem
 cat <<EOF >/etc/systemd/system/home2.automount
@@ -89,29 +95,35 @@ EOF
 
 systemctl daemon-reload
 systemctl start home2.automount
-echo $'\n'"{Info} getting status of systemd unit home2.mount"
+echo $'\n\E[0;33;44m'"{Info} getting status of systemd unit home2.mount"$'\E[0m'
 systemctl status home2.mount | grep Active:
 
 nfsmp=/mnt/nfsmp-$$
 mkdir -p $nfsmp
-echo $'\n'"{Info} mount localhost:/ $nfsmp"
+echo $'\n\E[0;33;44m'"{Info} mount localhost:/ $nfsmp"$'\E[0m'
 mount localhost:/ $nfsmp
-mount -t nfs,nfs4 | grep $nfsmp
-echo $'\n'"{Info} ls -l $nfsmp"
+
+echo $'\n\E[0;33;44m'"{Info} ls -l $nfsmp"$'\E[0m'
 ls -l $nfsmp
 
-echo $'\n'"{Info} umount $nfsmp"
+echo $'\n\E[0;33;44m'"{Info} mount -t nfs,nfs4 | grep $nfsmp"$'\E[0m'
+mount -t nfs,nfs4 | grep $nfsmp
+
+echo $'\n\E[0;33;44m'"{Info} ls -l $nfsmp"$'\E[0m'
+ls -l $nfsmp
+
+echo $'\n\E[0;33;44m'"{Info} umount $nfsmp"$'\E[0m'
 { umount $nfsmp || umount -fl $nfsmp; } && rm -rf $nfsmp
 if mountpoint $nfsmp 2>/dev/null; then
 	systemctl stop home2.mount
 	{ umount $nfsmp || umount -fl $nfsmp; } && rm -rf $nfsmp
 fi
 
-echo $'\n'"{Info} getting status of systemd unit home2.mount again"
+echo $'\n\E[0;33;44m'"{Info} getting status of systemd unit home2.mount again"$'\E[0m'
 systemctl status home2.mount | grep Active:
 if systemctl status home2.mount | grep -q mounted; then
-	echo $'\n'"{Info} stop systemd unit home2.mount"
+	echo $'\n\E[0;33;44m'"{Info} stop systemd unit home2.mount"$'\E[0m'
 	systemctl stop home2.mount
-	echo $'\n'"{Info} umount /home2"
+	echo $'\n\E[0;33;44m'"{Info} umount /home2"$'\E[0m'
 	umount /home2
 fi
