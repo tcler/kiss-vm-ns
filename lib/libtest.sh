48c2c43eeea7d05f8139dec898e07c71da212760
Jianhong Yin <yin-jianhong@163.com>
2022-12-26 23:45:18 -0500

    lib/libtest.sh: add curl_download()
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- lib/libtest.sh
+++ lib/libtest.sh
@@ -193,6 +193,43 @@ run() {
 trun() { run -d "$@"; }
 xrc() { chkrc "$@"; }
 
+curl_download() {
+	local filename=$1
+	local url=$2
+	shift 2;
+
+	local curlopts="-f -L -k"
+	local header=
+	local fsizer=1
+	local fsizel=0
+	local rc=
+
+	[[ -z "$filename" || -z "$url" ]] && {
+		echo "Usage: curl_download <filename> <url> [curl options]" >&2
+		return 1
+	}
+
+	header=$(curl -L -I -s $url|sed 's/\r//')
+	fsizer=$(echo "$header"|awk '/Content-Length:/ {print $2; exit}')
+	if echo "$header"|grep -q 'Accept-Ranges: bytes'; then
+		curlopts+=' --continue-at -'
+	fi
+
+	echo "{VM:INFO} run: curl -o $filename \$url $curlopts $curlOpt $@"
+	run -as=$VMUSER curl -o $filename $url $curlopts $curlOpt "$@"
+	rc=$?
+	if [[ $rc != 0 && -s $filename ]]; then
+		fsizel=$(stat --printf %s $filename)
+		if [[ $fsizer -le $fsizel ]]; then
+			echo "{VM:INFO} *** '$filename' already exist $fsizel/$fsizer"
+			rc=0
+		fi
+	fi
+
+	return $rc
+}
+curl_download_x() { until curl_download "$@"; do sleep 1; done; }
+
 #return if I'm being sourced
 (return 0 2>/dev/null) && sourced=yes || sourced=no
 if [[ $sourced = yes ]]; then return 0; fi
