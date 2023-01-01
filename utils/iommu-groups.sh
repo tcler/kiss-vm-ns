fd492dcce6b5e31e5a3e2a37a5d2bb50abdb4111
Jianhong Yin <yin-jianhong@163.com>
2023-01-01 12:40:54 +0800

    utils/iommu-groups.sh: add check if iommu is enabled
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/iommu-groups.sh
+++ utils/iommu-groups.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 iommuGrpsRoot=/sys/kernel/iommu_groups
+if [[ -z "$(ls $iommuGrpsRoot)" ]]; then
+	cat <<-\EOF >&2
+		{WARN} your host has not enabled iommu feature, please try follow command and reboot:
+		   sudo grubby --args="intel_iommu=on iommu=pt" --update-kernel="$(/sbin/grubby --default-kernel)"
+	EOF
+	exit 1
+fi
 
 shopt -s nullglob
 for grp in $(ls $iommuGrpsRoot | sort -V); do
