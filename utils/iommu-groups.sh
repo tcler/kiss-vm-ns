8ef8caec80fd3f9e4a1e2f2ada1f257dd2f507f4
Jianhong Yin <jiyin@redhat.com>
2023-01-05 18:31:20 +0800

    utils/iommu-groups.sh: fix kernel opts on amd platform
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/iommu-groups.sh
+++ utils/iommu-groups.sh
@@ -2,12 +2,23 @@
 
 iommuGrpsRoot=/sys/kernel/iommu_groups
 if [[ -z "$(ls $iommuGrpsRoot)" ]]; then
-	cat <<-\EOF >&2
-		{WARN} your host has not enabled iommu feature, please try follow command and reboot:
-		   sudo grubby --args="intel_iommu=on iommu=pt" --update-kernel="$(/sbin/grubby --default-kernel)"
-		   #or
-		   sudo grubby --args="intel_iommu=on iommu=pt" --update-kernel=DEFAULT
-	EOF
+	if grep -q AuthenticAMD /proc/cpuinfo; then
+		kernelopts="amd_iommu=on"
+	elif grep -q GenuineIntel /proc/cpuinfo; then
+		kernelopts="intel_iommu=on iommu=pt"
+	fi
+
+	if [[ -n "$kernelopts" ]]; then
+		cat <<-EOF >&2
+			{WARN} your host has not enabled iommu feature, please try follow command and reboot:
+			   sudo grubby --args="$kernelopts" --update-kernel=DEFAULT
+			   #or
+			   sudo sed -i '/'"$kernelopts"'/!{/GRUB_CMDLINE_LINUX/s/"$/'" $kernelopts"'"/}' /etc/default/grub
+			   sudo grub2-mkconfig | sudo tee /boot/grub2/grub.cfg /boot/efi/EFI/fedora/grub.cfg
+		EOF
+	else
+		echo "{WARN} your host has not enabled iommu feature."
+	fi
 	exit 1
 fi
 
