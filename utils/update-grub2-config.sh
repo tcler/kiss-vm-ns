3b9c6a76ce4a175e264b56d0886c7e1591389015
Jianhong Yin <jiyin@redhat.com>
2023-01-06 10:54:30 +0800

    utils: add update-grub2-config.sh
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

---
+++ utils/update-grub2-config.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+switchroot() {
+	local P=$0 SH=; [[ $0 = /* ]] && P=${0##*/}; [[ -e $P && ! -x $P ]] && SH=$SHELL
+	[[ $(id -u) != 0 ]] && {
+		echo -e "\E[1;30m{WARN} $P need root permission, switch to:\n  sudo $SH $P $@\E[0m"
+		exec sudo $SH $P "$@"
+	}
+}
+switchroot "$@"
+
+grub2-mkconfig | tee $(find /boot -name grub.cfg)
