2b1edbf5ec422a20f632395df3b114c41d81d794
Jianhong Yin <jiyin@redhat.com>
2023-01-07 15:07:20 +0800

    utils/syscall-check.sh: optimize code
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/syscall-check.sh
+++ utils/syscall-check.sh
@@ -5,11 +5,33 @@ rpm -q gcc strace >/dev/null || {
 }
 
 # check if syscall is implemented
-while read snum sname; do [[ $snum != [0-9]* || $sname = vhangup ]] && continue;  echo -e '#define _GNU_SOURCE\n#include <unistd.h>\n#include <sys/syscall.h>\nint main (void) { syscall ('$snum', 0, 0, 0, 0, 0, 0, 0, 0); }'|LANG=C gcc -x c -; LANG=C timeout 2 strace -e $sname ./a.out ;  done < <(ausyscall --dump)  |& grep ENOSYS
+while read snum sname; do
+	[[ $snum != [0-9]* || $sname = vhangup ]] && continue;
+	cat <<-CFILE | LANG=C gcc -x c -
+		#define _GNU_SOURCE
+		#include <unistd.h>
+		#include <sys/syscall.h>
+		int main (void) {
+			syscall($snum, 0, 0, 0, 0, 0, 0, 0, 0);
+		}
+	CFILE
+
+	LANG=C timeout 2 strace -e $sname ./a.out ;
+done < <(ausyscall --dump) |& grep ENOSYS
 rm -f a.out
 
 echo
 
 # check if there is syscall wrapper in libc
-while read snum sname; do [[ $snum != [0-9]* ]] && continue;  echo -e '#define _GNU_SOURCE\n#include <unistd.h>\n#include <sys/syscall.h>\nint main (void) { syscall (SYS_'$sname', 0, 0, 0, 0, 0, 0, 0, 0); }'|LANG=C gcc -x c -;  done < <(ausyscall --dump) |& grep error:
+while read snum sname; do
+	[[ $snum != [0-9]* ]] && continue;
+	cat <<-CFILE | LANG=C gcc -x c -
+		#define _GNU_SOURCE
+		#include <unistd.h>
+		#include <sys/syscall.h>
+		int main (void) {
+			syscall(SYS_$sname, 0, 0, 0, 0, 0, 0, 0, 0);
+		}
+	CFILE
+done < <(ausyscall --dump) |& grep error:
 rm -f a.out
