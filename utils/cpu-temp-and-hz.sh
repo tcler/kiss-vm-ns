eb23ff9ad56d5637b8e8eb5206708b4f75811747
Jianhong Yin <jiyin@redhat.com>
2023-02-27 21:39:51 +0800

    utils/cpu-temp-and-hz.sh: output all zone's temp
    there are multi zones on my another Intel platform
    
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/cpu-temp-and-hz.sh
+++ utils/cpu-temp-and-hz.sh
@@ -4,7 +4,12 @@ watch -t -n 1 '
 	date
 	echo
 	if test -f /sys/class/thermal/thermal_zone0/temp; then
-		echo CPU: $(($(cat /sys/class/thermal/thermal_zone0/temp 2>/dev/null||echo 0)/1000))°
+		tempUnit=$"\\U2103"; [[ "$LANG" = C ]] && tempUnit=^C
+		echo -n "CPU temp: {"
+		for tempf in /sys/class/thermal/thermal_zone*; do
+			echo -en "${tempf##*/thermal_}: $(($(cat $tempf/temp 2>/dev/null||echo 0)/1000))$tempUnit,"
+		done
+		echo "}"
 	else
 		sensors|sed -n "/Physical id 0/{s/[^+]*+//; p;}"
 	fi
