8bfaaa600455cb4f8795405cf01b699adfb04dfa
Jianhong Yin <jiyin@redhat.com>
2023-03-09 10:25:24 +0800

    utils: add enable-udisksctl-loop-setup-and-delete.sh
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

---
+++ utils/enable-udisksctl-loop-setup-and-delete.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#let user do udisksctl loop-setup/loop-delete without password
+#
+LANG=C
+switchroot() {
+	local P=$0 SH=; [[ $0 = /* ]] && P=${0##*/}; [[ -e $P && ! -x $P ]] && SH=$SHELL
+	[[ $(id -u) != 0 ]] && {
+		echo -e "\E[1;30m{WARN} $P need root permission, switch to:\n  sudo $SH $P $@\E[0m"
+		exec sudo $SH $P "$@"
+	}
+}
+
+#__main__
+switchroot "$@"
+
+#change policy org.freedesktop.UDisks2.policy or org.freedesktop.udisks2.policy on debian
+#see also: https://lists.debian.org/debian-devel/2017/01/msg00081.html
+sed -ri -e '/(filesystem-mount"|loop-setup|loop-delete)/,/<\/action>/{s/auth_admin[_a-z]*\>/yes/}' \
+	/usr/share/polkit-1/actions/org.freedesktop.??isks2.policy
