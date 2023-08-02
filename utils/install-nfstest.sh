90e7a7698bbc2cb1434d08d6866b2cdc0ebf9e73
Jianhong Yin <jiyin@redhat.com>
2023-08-02 17:26:32 +0800

    utils: add install-nfstest.sh
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

---
+++ utils/install-nfstest.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#Author: Jianhong Yin <yin-jianhong@163.com>
+#for: install nfstest from source code
+
+_url='http://git.linux-nfs.org/?p=mora/nfstest.git;a=snapshot;h=HEAD;sf=tgz'
+_tarf=nfstest.tgz
+_xdir=nfstest
+targetdir=/usr/src; [[ $(id -u) != 0 ]] && { targetdir=${HOME}/src; }
+mkdir -p ${targetdir}/$_xdir
+
+#install and extract
+echo "{info} install-nfstest from '$_url'"
+curl -k -Ls "$_url" -o ${targetdir}/$_tarf
+pushd $targetdir &>/dev/null
+	echo "{info} extract $_tarf to $targetdir/$_xdir  "
+	tar -C $_xdir -zxf $_tarf --strip-components=1
+	sed -ri -e 's/(cmd.*)(-w %s host %s)(.*, )self.tracefile, hosts/\1-w - host %s|cat >%s\3hosts, self.tracefile/' nfstest/nfstest/host.py
+popd &>/dev/null
+
+#export env
+_envf=/tmp/nfstest.env
+cat <<-EOF >$_envf
+export PYTHONPATH=$targetdir/$_xdir
+export PATH=$targetdir/$_xdir/test:$PATH
+EOF
+echo "{info} please source '$_envf', and run your tests"
