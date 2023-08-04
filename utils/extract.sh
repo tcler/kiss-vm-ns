21a062f4f6dc726d62dc3cb9656534c07e41ed83
Jianhong Yin <jiyin@redhat.com>
2023-08-04 23:24:03 +0800

    utils/extract.sh: handle the case no topdir in the compressed file
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/extract.sh
+++ utils/extract.sh
@@ -18,10 +18,15 @@ targetdir=${2:-.}
 topdir=$3
 
 filetype=$(file -b ${compressedFile})
+_targetdir=$targetdir
 if [[ "$filetype" = Zip* ]]; then
-	otopdir=$(unzip -Z1 $filepath | sed '1{p;q}')
+	otopdir=($(unzip -Z1 $filepath | awk -F/ '{a[$1]++} END { for(key in a) { print(key) } }'))
 	[[ -z "$otopdir" ]] && { echo "{error} extract $compressedFile fail" >&2; exit 3; }
-	unzip "$compressedFile" -d $targetdir &>/dev/null
+	[[ "${#otopdir[@]}" -gt 1 ]] && {
+		otopdir=; [[ -n "$topdir" ]] && _targetdir+=/$topdir; topdir=
+		[[ "$_targetdir" != $targetdir ]] && { mkdir -p $_targetdir; }
+	}
+	unzip "$compressedFile" -d $_targetdir &>/dev/null
 else
 	case "$filetype" in
 		(gzip*) xtype=z;;
@@ -29,12 +34,16 @@ else
 		(XY*) xtype=J;;
 		(*) xtype=a;;
 	esac
-	otopdir=$(tar taf ${compressedFile} | sed -n '1{s@/$@@;p;q}')
+	otopdir=($(tar taf ${compressedFile} | awk -F/ '{a[$1]++} END { for(key in a) { print(key) } }'))
 	[[ -z "$otopdir" ]] && { echo "{error} extract $compressedFile fail" >&2; exit 3; }
-	tar -C $targetdir -${xtype}xf ${compressedFile} #--strip-components=1
+	[[ "${#otopdir[@]}" -gt 1 ]] && {
+		otopdir=; [[ -n "$topdir" ]] && _targetdir+=/$topdir; topdir=
+		[[ "$_targetdir" != $targetdir ]] && { mkdir -p $_targetdir; }
+	}
+	tar -C $_targetdir -${xtype}xf ${compressedFile} #--strip-components=1
 fi
-[[ -d "$targetdir/${otopdir}" ]] || {
-	echo "{Error} extract to '$targetdir' fail, please theck permission" >&2
+[[ -d "$_targetdir/${otopdir}" ]] || {
+	echo "{Error} extract to '$_targetdir' fail, please theck permission" >&2
 	exit 1
 }
-[[ -n "$topdir" && "${topdir}" != "$otopdir" ]] && mv $targetdir/${otopdir} $targetdir/$topdir
+[[ -n "$topdir" && "${topdir}" != "$otopdir" ]] && mv $_targetdir/${otopdir} $_targetdir/$topdir
