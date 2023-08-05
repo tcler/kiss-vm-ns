b1a28e1830c97a4e8d79a94161c1e3d54ac802b6
Jianhong Yin <yin-jianhong@163.com>
2023-08-05 22:00:14 +0800

    utils/extract.sh: support extract specific sub-folders
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/extract.sh
+++ utils/extract.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
+
+## argparse
+P=${0##*/}
+Usage() {
+	echo "Usage: $P <compressed-file> [targetdir] [topdirname] [--path]"
+	echo "  e.g: $P /path/to/kernel-src.tar.gz /usr/src kernel-devel"
+	echo "  e.g: $P /path/to/kernel-src.tar.gz /usr/src kernel-doc --path=Documentation/*"
+}
+_at=`getopt -o hp: \
+	--long help \
+	--long path: \
+    -a -n '$P' -- "$@"`
+eval set -- "$_at"
+
+folders=
+while true; do
+	case "$1" in
+	-h|--help)      Usage; shift 1; exit 0;;
+	-p|--path)      folders="$2"; shift 2;;
+	--) shift; break;;
+	esac
+done
+
 [[ $# = 0 ]] && {
-	echo "{Usage} $0 <compressed-file> [targetdir] [topdirname]" >&2
+	Usage >&2
 	exit 1
 }
 
@@ -26,7 +49,7 @@ if [[ "$filetype" = Zip* ]]; then
 		otopdir=; [[ -n "$topdir" ]] && _targetdir+=/$topdir; topdir=
 		[[ "$_targetdir" != $targetdir ]] && { mkdir -p $_targetdir; }
 	}
-	unzip "$compressedFile" -d $_targetdir &>/dev/null
+	unzip "$compressedFile" $folders -d $_targetdir &>/dev/null
 else
 	case "$filetype" in
 		(gzip*) xtype=z;;
@@ -40,10 +63,16 @@ else
 		otopdir=; [[ -n "$topdir" ]] && _targetdir+=/$topdir; topdir=
 		[[ "$_targetdir" != $targetdir ]] && { mkdir -p $_targetdir; }
 	}
-	tar -C $_targetdir -${xtype}xf ${compressedFile} #--strip-components=1
+	_cmd="tar -C $_targetdir -${xtype}xf ${compressedFile} $folders"   #--strip-components=1
+	echo "{run} $_cmd" >&2
+	eval $_cmd
 fi
 [[ -d "$_targetdir/${otopdir}" ]] || {
 	echo "{Error} extract to '$_targetdir' fail, please theck permission" >&2
 	exit 1
 }
-[[ -n "$topdir" && "${topdir}" != "$otopdir" ]] && mv $_targetdir/${otopdir} $_targetdir/$topdir
+[[ -n "$topdir" && "${topdir}" != "$otopdir" ]] && {
+	_cmd="mv $_targetdir/${otopdir} $_targetdir/$topdir"
+	echo "{run} $_cmd" >&2
+	eval $_cmd
+}
