55c6a31b705f6be9c9633bdb8ef8e75b28c73db9
Jianhong Yin <jiyin@redhat.com>
2023-08-22 13:53:50 +0800

    utils: add pynfs-install.sh,nfstest-install.sh symlink
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/nfstest-install.sh
+++ utils/nfstest-install.sh
@@ -1,17 +1,1 @@
-#!/bin/bash
-
-gitrepo=git://git.linux-nfs.org/projects/mora/nfstest.git
-
-[[ -f /usr/bin/git ]] || {
-	yum install -y git
-}
-
-echo "{nfstest-install} from $gitrepo"
-git clone $gitrepo || exit $?
-export PYTHONPATH=$PWD/nfstest
-export PATH=$PWD/nfstest/test:$PATH
-
-cat <<-EOF >nfstest.env
-export PYTHONPATH=$PWD/nfstest
-export PATH=$PWD/nfstest/test:$PATH
-EOF
+install-nfstest.sh
