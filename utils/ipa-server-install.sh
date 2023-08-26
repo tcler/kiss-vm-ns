4f68a543fa6d9c5b19a839ec3947e2c3c8f01744
Jianhong Yin <jiyin@redhat.com>
2023-08-27 00:47:47 +0800

    utils/ipa-*-install.sh: add --nobest --allowerasing to yum install
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/ipa-server-install.sh
+++ utils/ipa-server-install.sh
@@ -20,31 +20,38 @@ if [[ $OSV != %rhel && $OSV -lt 6 ]]; then
 fi
 
 switchroot "$@"
+yOpt="--nobest --allowerasing -q"
 
 echo "{INFO} installing ipa-server ..."
-case $OSV in
-6|7)
-	#https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux/7/html-single/linux_domain_identity_authentication_and_policy_guide/index#required-packages
-	#yum install -y ipa-server                   #IdM server without an integrated DNS
-	yum install -y -q ipa-server ipa-server-dns     #IdM server with an integrated DNS
-	;;
-8)
-	#https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux/8/html-single/installing_identity_management/index#installing-packages-required-for-an-idm-server_preparing-the-system-for-ipa-server-installation
-	umask 0022
-	yum module enable -y idm:DL1
-	yum distro-sync -y
-	#yum module install -y idm:DL1/server        #IdM server without an integrated DNS
-	#yum module install -y idm:DL1/adtrust       #IdM server that has a trust agreement with Active Directory
-	#yum module install -y idm:DL1/{dns,adtrust}
-	yum module install -y -q idm:DL1/dns            #IdM server with an integrated DNS
-	umask 0027
-	;;
-9|*)
-	#https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux/9/html-single/installing_identity_management/index#installing-packages-required-for-an-idm-server_preparing-the-system-for-ipa-server-installation
-	umask 0022
-	#yum install -y ipa-server ipa-server-trust-ad samba-client   #IdM server that has a trust agreement with Active Directory
-	#yum install -y ipa-server                   #IdM server without an integrated DNS
-	yum install -y -q ipa-server ipa-server-dns     #IdM server with an integrated DNS
-	umask 0027
-	;;
-esac
+#fedora
+if [[ "$OSV" =~ %rhel ]]; then
+	dnf install -y $yOpt freeipa-server freeipa-server-dns  #IdM server with an integrated DNS
+#rhel/centos/rocky/...
+else
+	case $OSV in
+	6|7)
+		#https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux/7/html-single/linux_domain_identity_authentication_and_policy_guide/index#required-packages
+		#yum install -y $yOpt ipa-server                #IdM server without an integrated DNS
+		yum install -y $yOpt ipa-server ipa-server-dns  #IdM server with an integrated DNS
+		;;
+	8)
+		#https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux/8/html-single/installing_identity_management/index#installing-packages-required-for-an-idm-server_preparing-the-system-for-ipa-server-installation
+		umask 0022
+		yum module enable -y idm:DL1
+		yum distro-sync -y
+		#yum module install -y $yOpt idm:DL1/server     #IdM server without an integrated DNS
+		#yum module install -y $yOpt idm:DL1/adtrust    #IdM server that has a trust agreement with Active Directory
+		#yum module install -y $yOpt idm:DL1/{dns,adtrust}
+		yum module install -y $yOpt idm:DL1/dns         #IdM server with an integrated DNS
+		umask 0027
+		;;
+	9|*)
+		#https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux/9/html-single/installing_identity_management/index#installing-packages-required-for-an-idm-server_preparing-the-system-for-ipa-server-installation
+		umask 0022
+		#yum install -y $yOpt ipa-server ipa-server-trust-ad samba-client   #IdM server that has a trust agreement with Active Directory
+		#yum install -y $yOpt ipa-server                #IdM server without an integrated DNS
+		yum install -y $yOpt ipa-server ipa-server-dns  #IdM server with an integrated DNS
+		umask 0027
+		;;
+	esac
+fi
