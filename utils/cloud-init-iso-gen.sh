0a88939b962f8599ad45819c2c990eb72e670347
Jianhong Yin <jiyin@redhat.com>
2023-09-07 15:45:57 +0800

    Update the cert files url
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/cloud-init-iso-gen.sh
+++ utils/cloud-init-iso-gen.sh
@@ -147,7 +147,7 @@ runcmd:
 $(
 if [[ $Intranet = yes ]]; then
 cat <<IntranetCMD
-  - (cd /etc/pki/ca-trust/source/anchors && curl -Ls --remote-name-all https://password.corp.redhat.com/{RH-IT-Root-CA.crt,legacy.crt} && update-ca-trust)
+  - (cd /etc/pki/ca-trust/source/anchors && curl -Ls --remote-name-all https://certs.corp.redhat.com/{2022-IT-Root-CA.pem,2015-IT-Root-CA.pem,ipa.crt} && update-ca-trust)
   - command -v yum && (cd /usr/bin && curl -L -k -m 30 --remote-name-all $bkrClientImprovedUrl/utils/{brewinstall.sh,taskfetch.sh} && chmod +x brewinstall.sh taskfetch.sh) &&
     { taskfetch.sh --install-deps; brewinstall.sh $(for b in $BPKGS; do echo -n "'$b' "; done) -noreboot; }
 
