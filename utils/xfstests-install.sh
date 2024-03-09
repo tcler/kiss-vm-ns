786b76a0069000ead21d1092b235b609758f79f1
Jianhong Yin <jiyin@redhat.com>
2024-03-09 21:18:52 +0800

    utils/xfstests-install.sh: use curl download instead git clone
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/xfstests-install.sh
+++ utils/xfstests-install.sh
@@ -16,14 +16,14 @@ nouring=$1
 # clone xfstests in background
 command -v git || _deps=git; command -v tmux || _deps+=" tmux"
 [[ -n "$_deps" ]] && yum install -y $_deps
-pkg=xfstests
-gitUrl=git://git.kernel.org/pub/scm/fs/xfs/xfstests-dev.git
-backupUrl=https://github.com/kdave/xfstests
-cloneSession=clone-xfstests-$$
-tmux new -s $cloneSession -d "git clone $gitUrl $pkg || git clone $backupUrl $pkg"
+tgzUrl=https://git.kernel.org/pub/scm/fs/xfs/xfstests-dev.git/snapshot/xfstests-dev-master.tar.gz
+backupUrl=https://github.com/kdave/xfstests/archive/refs/heads/master.tar.gz
+downloadSession=download-xfstests-$$
+tmux new -s $downloadSession -d "{ curl -LO $tgzUrl && tar axf ${tgzUrl##*/} || curl -LO $backupUrl && tar axf ${backupUrl##*/}; }"
 
 #Prepare: install deps
-yum install -y acl attr automake bc dbench dump e2fsprogs fio gawk gcc \
+yum clean packages
+yum install -y --nogpgcheck acl attr automake bc dbench dump e2fsprogs fio gawk gcc \
 	gdbm-devel git indent kernel-devel libacl-devel \
 	libcap-devel libtool libuuid-devel lvm2 make psmisc \
 	python3 quota sed sqlite udftools xfsprogs xfsprogs-devel
@@ -35,10 +35,11 @@ grep -q CONFIG_AIO=y /boot/config-$(uname -r) && yum install -y libaio-devel
 }
 
 # wait clone finish
-while tmux ls | grep $cloneSession; do sleep 8; done
+while tmux ls | grep $downloadSession; do sleep 8; done
 
 #Install form src
-cd $pkg && make && make install
+dir=xfstests-dev-master; test -d $dir || dir=xfstests-master
+cd $dir && make && make install
 
 # Check install result
 ls -lF /var/lib/xfstests/
