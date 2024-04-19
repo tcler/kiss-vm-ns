b6c26e2bcc0c7fe094fd338f394706f93fd15b2b
Jianhong Yin <jiyin@redhat.com>
2024-04-19 16:21:55 +0800

    utils/addbridge.sh: more exception handling code
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/addbridge.sh
+++ utils/addbridge.sh
@@ -8,6 +8,7 @@ switchroot() {
 	}
 }
 
+is_bridge() { local ifname=$1; ip -d a s "$ifname" | grep -qw bridge; }
 is_slave() {
 	local ifname=$1
 	read key br < <(ip addr show dev $ifname | grep -Eo 'master [^ ]+')
@@ -18,29 +19,36 @@ is_slave() {
 #__main__
 switchroot "$@"
 
+_at=()
+for arg; do [[ "$arg" = -f ]] && force=yes || _at+=("$arg"); done
+set -- "${_at[@]}"
 brname=${1}
 ifname=${2}
 
 brname=${brname:-br0}
 [[ -z "$ifname" ]] && ifname=$(get-default-if.sh)
 [[ -z "$ifname" ]] && {
-	echo "{warn} there's not bridge-slave ifname, and auto detect fail." >&2
+	echo "{warn} there is no bridge-slave ifname specified, and auto detect fail." >&2
 	echo "{info} Usage: $0 [bridge name] [bridge-slave ifname]" >&2
 	exit 1
 }
 
 if ip addr show dev $brname &>/dev/null; then
-	echo "{warn} network interface '$brname' has been there" >&2
+	echo "{warn} bridge dev '$brname' has been there" >&2
 	exit 1
 fi
 if br=$(is_slave $ifname); then
 	echo "{warn} network interface '$ifname' has been a bridge-slave of '$br'" >&2
 	exit 1
 fi
+if is_bridge $ifname && [[ "$force" != yes ]]; then
+	echo "{warn} network interface '$ifname' is bridge device, and -f option you really want nested bridge device?" >&2
+	exit 1
+fi
 
 coname=$(nmcli -g GENERAL.CONNECTION device show $ifname)
 nmcli c delete "$coname" 2>/dev/null
-nmcli c delete "co-$ifname" 2>/dev/null
 nmcli c add type bridge ifname $brname stp off autoconnect yes
 nmcli c add type bridge-slave ifname "$ifname" master $brname autoconnect yes
+
 systemctl restart NetworkManager
