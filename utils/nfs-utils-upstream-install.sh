9389dd0b9e497a65c05442de751bf265f1de3566
Jianhong Yin <jiyin@redhat.com>
2024-05-17 14:46:14 +0800

    utils/nfs-utils-upstream.sh: support multi platform
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/nfs-utils-upstream-install.sh
+++ utils/nfs-utils-upstream-install.sh
@@ -3,9 +3,17 @@
 RC=1
 gitUrl=git://git.linux-nfs.org/projects/steved/nfs-utils.git
 
-[[ -f /usr/bin/git ]] || {
-	yum install -y git
-}
+if ! { command -v git && command -v make; }; then
+	if command -v dnf &>/dev/null; then
+		dnf install -y git make
+	elif command -v apt &>/dev/null; then
+		apt install -o APT::Install-Suggests=0 -o APT::Install-Recommends=0 -y git make
+	elif command -v zypper &>/dev/null; then
+		zypper in --no-recommends -y git make
+	elif command -v pacman &>/dev/null; then
+		pacman -Sy --noconfirm git make
+	fi
+fi
 
 if git clone $gitUrl; then
 	pushd nfs-utils
