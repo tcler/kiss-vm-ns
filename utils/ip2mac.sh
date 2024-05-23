c1a63ef98927dcd7691214d7b5214c51d37c1952
Jianhong Yin <jiyin@redhat.com>
2024-05-23 13:25:26 +0800

    utils/ip2mac.sh: add code for that conn not exist, and fix bug
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/ip2mac.sh
+++ utils/ip2mac.sh
@@ -26,6 +26,13 @@ reip='^([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])'
 [[ "$ipaddr" =~ $remac && "$mac" =~ $reip ]] && read ipaddr mac <<<"$mac $ipaddr"
 [[ "$ipaddr" =~ .*/[0-9]+$ ]] || ipaddr+=/24
 
-ifname_=$(ip -o link | awk "/$mac/{print \$2}")
-coname=$(nmcli -g GENERAL.CONNECTION device show ${ifname_%:})
-nmcli con mod "${coname}" ipv4.addresses $ipaddr
+ifname_=$(ip -o link | awk "/$mac/{print \$2}"); ifname=${ifname_%:}
+#coname=$(nmcli -g GENERAL.CONNECTION device show ${ifname})
+#[[ -z "$coname" ]] && { coname=con-$ifname; nmcli con add type ethernet ifname eth1 con-name $coname; }
+while :; do
+	coname=$(nmcli -g GENERAL.CONNECTION device show ${ifname})
+	#[[ -z "$coname" ]] && { coname=con-$ifname; nmcli con add type ethernet ifname eth1 con-name $coname; }
+	[[ -z "$coname" ]] && sleep 2 || break
+done
+nmcli con mod "${coname}" ipv4.addresses $ipaddr ipv4.method manual
+nmcli con up "${coname}"
