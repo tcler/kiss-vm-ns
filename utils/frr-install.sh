fc03203f1d5026c27de3b7ca4e6cec552bf10a25
Jianhong Yin <jiyin@redhat.com>
2024-05-23 16:28:59 +0800

    utils/frr-install.sh: install frr from default RHEL repo
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/frr-install.sh
+++ utils/frr-install.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-FRRVER=frr-stable
+#workaround for that no nameserver in /etc/resolv.conf
+grep -q ^nameserver /etc/resolv.conf || ip r|awk '/^default/{print "nameserver", $3}' >>/etc/resolv.conf
+
+FRRVER=${FRRVER:-frr-stable}
 if command -v yum; then
 	verx=$(rpm -E %rhel)
-	if [[ "$verx" != %rhel ]]; then
+	#since RHEL-8, RHEL has provided frr package by default
+	if [[ "$verx" != %rhel && "$verx" -le 7 ]]; then
 		frrRepoRpmUrl=https://rpm.frrouting.org/repo/$FRRVER-repo-1-0.el${verx}.noarch.rpm
 		sudo yum install -y $frrRepoRpmUrl  #${FRRVER}*
 	fi
 	sudo yum install --setopt=strict=0 -y frr frr-pythontools
+	systemctl enable frr; systemctl start frr
 fi
