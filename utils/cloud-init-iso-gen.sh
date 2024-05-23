64713c27c9a534186a42bdcbaebb04b1729b8a5c
Jianhong Yin <jiyin@redhat.com>
2024-05-23 15:49:45 +0800

    fix that no nameserver in /etc/resolv.conf
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/cloud-init-iso-gen.sh
+++ utils/cloud-init-iso-gen.sh
@@ -192,6 +192,7 @@ $(
 cat <<DNS_DOMAIN
   - hostn=\$(hostname); domain=\${hostn#*.}; grep -q "search .* \${domain}" /etc/resolv.conf && sed -i -e "/^search/{s/ \${domain}//;s/search/& \${domain}/}" /etc/resolv.conf
 DNS_DOMAIN
+  - grep ^nameserver /etc/resolv.conf || ip r|awk '/^default/{print "nameserver", \$3}' >>/etc/resolv.conf
 [[ -n "$defaultDNS" ]] && cat <<DNS
   - grep -q systemd-resolved /etc/resolv.conf || { sed -i -e "/$defaultDNS/d" -e "0,/nameserver/s//nameserver $defaultDNS\n&/" /etc/resolv.conf; sed -ri '/^\[main]/s//&\ndns=none\nrc-manager=unmanaged/' /etc/NetworkManager/NetworkManager.conf; }
   - cp /etc/resolv.conf{,.new}
