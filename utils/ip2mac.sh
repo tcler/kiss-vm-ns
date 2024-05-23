abf30bc1b89627f557e9178a372be2597f502d48
Jianhong Yin <jiyin@redhat.com>
2024-05-23 12:03:11 +0800

    utils/ip2mac.sh: identify the address type(ip or mac) and fix typo
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/ip2mac.sh
+++ utils/ip2mac.sh
@@ -16,13 +16,15 @@ for arg; do [[ "$arg" = -f ]] && force=yes || _at+=("$arg"); done
 set -- "${_at[@]}"
 ipaddr=${1}
 mac=${2}
+remac='^([a-fA-F0-9]{2}:){5}[a-fA-F0-9]{2}$'
+reip='^([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])'
 
 [[ -z "$ipaddr" || -z "$mac" ]] && {
 	echo "Usage: $0 <ip-address> <mac-addr>" >&2
 	exit 1
 }
-
-[[ "$ipaddr" =~ .*/[0=9]+$ ]] || ipaddr+=/24
+[[ "$ipaddr" =~ $remac && "$mac" =~ $reip ]] && read ipaddr mac <<<"$mac $ipaddr"
+[[ "$ipaddr" =~ .*/[0-9]+$ ]] || ipaddr+=/24
 
 ifname_=$(ip -o link | awk "/$mac/{print \$2}")
 coname=$(nmcli -g GENERAL.CONNECTION device show ${ifname_%:})
