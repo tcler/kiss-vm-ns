5571e7607ea987815e5dbc4b47152c7687decc3e
Jianhong Yin <jiyin@redhat.com>
2024-05-24 18:08:10 +0800

    examples/frr-ospf-example.sh: add more comments
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- examples/frr-ospf-example.sh
+++ examples/frr-ospf-example.sh
@@ -5,6 +5,7 @@
 [[ $1 != -* ]] && { distro="$1"; shift; }
 distro=${distro:-9}
 
+#mac-address and static-ip
 addr_user_r1=(02:00:aa:0a:01:02 10.10.100.1/24)
 addr_serv_r3=(02:00:aa:0b:03:02 10.10.200.1/24)
 addr_r1_user=(02:00:aa:01:0a:02 10.10.100.2/24)
@@ -16,13 +17,15 @@ addr_r3_r1=(02:00:aa:03:01:02 10.10.13.2/24)
 addr_r3_r2=(02:00:aa:03:02:03 10.10.23.2/24)
 addr_r3_serv=(02:00:aa:03:0b:04 10.10.200.2/24)
 
+#create virt network
 vm netcreate netname=net_user_r1 brname=br_user_r1 forward=no
 vm netcreate netname=net_r1_r2 brname=br_r1_r2 forward=no
 vm netcreate netname=net_r2_r3 brname=br_r2_r3 forward=no
 vm netcreate netname=net_r1_r3 brname=br_r1_r3 forward=no
 vm netcreate netname=net_r3_serv brname=br_r3_serv forward=no
-vm del user server r{1..3} &>/dev/null
 
+#create VM
+vm del user server r{1..3} &>/dev/null
 stdlog=$(vm create $distro --downloadonly "$@" |& tee /dev/tty)
 imgf=$(sed -rn '${/^-[-rwx]{9}.? /{s/^.* //;p}}' <<<"$stdlog")
 tmux new -s frr-user -d "vm create -n user ${distro} --net=default -p traceroute,tcpdump,nmap -nointeract -I=$imgf $@"
@@ -31,6 +34,7 @@ tmux new -s frr-r1 -d "vm create -n r1 ${distro} --net=default -nointeract -I=$i
 tmux new -s frr-r2 -d "vm create -n r2 ${distro} --net=default -nointeract -I=$imgf $@"
 vm create -n r3 ${distro} --net=default -nointeract -I=$imgf "$@"
 
+#attach interfaces to connecting the routers and workstations
 vm add.if user   net_user_r1 -- --mac=${addr_user_r1}
 vm add.if server net_r3_serv -- --mac=${addr_serv_r3}
 vm add.if r1     net_user_r1 -- --mac=${addr_r1_user}
@@ -42,12 +46,14 @@ vm add.if r3     net_r1_r3   -- --mac=${addr_r3_r1}
 vm add.if r3     net_r2_r3   -- --mac=${addr_r3_r2}
 vm add.if r3     net_r3_serv -- --mac=${addr_r3_serv}
 
+#waiting ssh available and copy tools in VMs
 for vm in user server r{1..3}; do
 	vm port-available -w $vm
 	vm cpto -v $vm /bin/ip2mac.sh /bin/frr-install.sh  /bin
 	vm exec -v $vm -- "systemctl stop firewalld; systemctl disable firewalld"
 done
 
+#apply static-ip to every network interface
 vm exec -v user -- ip2mac.sh ${addr_user_r1[@]}
 vm exec -v server -- ip2mac.sh ${addr_serv_r3[@]}
 vm exec -v r1 -- ip2mac.sh ${addr_r1_user[@]}
@@ -59,12 +65,14 @@ vm exec -v r3 -- ip2mac.sh ${addr_r3_r1[@]}
 vm exec -v r3 -- ip2mac.sh ${addr_r3_r2[@]}
 vm exec -v r3 -- ip2mac.sh ${addr_r3_serv[@]}
 
+#install frr on route VMs
 for vm in r{1..3}; do
 	vm exec -v $vm -- 'while ! rpm -q frr; do frr-install.sh; sleep 2; done'
 	vm exec -v $vm -- sysctl -w net.ipv4.ip_forward=1 net.ipv6.conf.all.forwarding=1
 	vm exec -v $vm -- sed -ri 's/(ospfd|zebra|staticd)=no/\1=yes/' /etc/frr/daemons
 done
 
+#configure frr on route VMs
 if_r1_user=$(vm exec r1 -- "ip -o link | awk -F'[ :]+' '/${addr_r1_user}/{print \$2}'")
 vm exec -v r1 -- 'cat >>/etc/frr/frr.conf <<EOF
 frr defaults traditional
@@ -119,13 +127,13 @@ line vty
 EOF
 systemctl reload frr'
 
-#add route for user and server
+#add route for user and server, avoid: test packet is routed to the default route(libvirt default net)
 if_user_r1=$(vm exec user -- "ip -o link | awk -F'[ :]+' '/${addr_user_r1}/{print \$2}'")
 if_serv_r3=$(vm exec server -- "ip -o link | awk -F'[ :]+' '/${addr_serv_r3}/{print \$2}'")
 vm exec -v user -- ip route add 10.10.0.0/16 via ${addr_r1_user[1]%/*} dev $if_user_r1 metric 99
 vm exec -v server -- ip route add 10.10.0.0/16 via ${addr_r3_serv[1]%/*} dev $if_serv_r3 metric 99
 
-#ping test
+#ping test before ospf route generated
 vm exec -v user -- ping -c 4  10.10.200.1
 vm exec -v user -- traceroute 10.10.200.1
 
