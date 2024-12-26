8767ec3ea3d28f07cd8e9609b4a699ec2c90346d
Jianhong Yin <jiyin@redhat.com>
2024-12-26 10:46:39 +0800

    improve switchroot() on macOS terminal
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/xfsprogs-upstream-install.sh
+++ utils/xfsprogs-upstream-install.sh
@@ -5,7 +5,7 @@
 switchroot() {
 	local P=$0 SH=; [[ $0 = /* ]] && P=${0##*/}; [[ -e $P && ! -x $P ]] && SH=$SHELL
 	[[ $(id -u) != 0 ]] && {
-		echo -e "\E[1;30m{WARN} $P need root permission, switch to:\n  sudo $SH $P $@\E[0m"
+		echo -e "\E[1;4m{WARN} $P need root permission, switch to:\n  sudo $SH $P $@\E[0m"
 		exec sudo $SH $P "$@"
 	}
 }
