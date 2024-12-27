ebe13eb36a3766aa802341d1ee2d28acbbec1c14
Jianhong Yin <jiyin@redhat.com>
2024-12-27 08:26:46 +0800

    utils/power-consumption.sh: call powerstat directly if installed
    and optimize output of powercap()
    
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/power-consumption.sh
+++ utils/power-consumption.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #autor: Jianhong Yin <yin-jianhong@163.com>
+#function: get cpu power consumption of current computer
 #ref: https://www.kernel.org/doc/html/next/power/powercap/powercap.html
 #ref: https://www.baeldung.com/linux/power-consumption
+#ref: https://www.onitroad.com/jc/faq/how-to-measure-power-consumption-using-powerstat-in-linux.html
 
 switchroot() {
 	local P=$0 SH=; [[ -x $0 && $0 = /* ]] && command -v ${0##*/} &>/dev/null && P=${0##*/}; [[ ! -f $P || ! -x $P ]] && SH=$SHELL
@@ -23,16 +25,28 @@ stime() {
 	case $unit in (*h) secs=$((timen*60*60));; (*m) secs=$((timen*60));; (*) secs=$timen;; esac
 	echo $secs
 }
+powercap() {
+	local time=${1,,}
+	local ZN=($(sudo cat /sys/class/powercap/*/name))      #Name of the power zone
+	local E0=($(sudo cat /sys/class/powercap/*/energy_uj)) #energy counter in micro joules at _time A
+	local stime=$(stime $time)
+	{
+	echo "Running for $time($stime seconds). then calculate the average power during this period"
+	for ((i=1; i <= stime; i++)); do case $((i%4)) in 0)p=/;; 1)p=-;; 2)p=\\;; 3)p=\|;; esac; echo -ne "\r$p $i"; sleep 1; done; echo
+	} >&2
+	E1=($(sudo cat /sys/class/powercap/*/energy_uj)) #energy counter in micro joules at _time A + $time
 
-time=${1:-8}
-[[ ${time,,} =~ ^[0-9]+[smh]?$ ]] || { echo -e "{ERR} '$time' is not a valid time string.\nUsage: $0 [during_time[smh]]"; exit 1; }
-ZN=($(sudo cat /sys/class/powercap/*/name))      #Name of the power zone
-E0=($(sudo cat /sys/class/powercap/*/energy_uj)) #energy counter in micro joules at _time A
-stime=$(stime $time)
-echo "{DEBUG} waiting $time($stime seconds). then calculate the average power during this period"
-sleep $time;
-E1=($(sudo cat /sys/class/powercap/*/energy_uj)) #energy counter in micro joules at _time A + $time
+	for i in "${!E0[@]}"; do
+		awk -v ZN=${ZN[i]} -v E1=${E1[i]} -v E0=${E0[i]} -v stime=$stime 'BEGIN { printf "%s: %.1f W\n", ZN, (E1-E0) / stime / 1e6 }'
+	done | awk '{ print; sum+=$2 } END { printf "[total]: %.1f W\n", sum }'
+}
 
-for i in "${!E0[@]}"; do
-	awk -v ZN=${ZN[i]} -v E1=${E1[i]} -v E0=${E0[i]} -v stime=$stime 'BEGIN { printf "%s: %.1f W\n", ZN, (E1-E0) / stime / 1e6 }'
-done
+if command -v powerstat &>/dev/null; then
+	powerstat_cmd="powerstat -R -c -z 5 12"
+	echo "{DEBUG} running: $powerstat_cmd"
+	$powerstat_cmd
+else
+	time=${1:-8}
+	[[ ${time,,} =~ ^[0-9]+[smh]?$ ]] || { echo -e "{ERR} '$time' is not a valid time string.\nUsage: $0 [during_time[smh]]"; exit 1; }
+	powercap ${time,,}
+fi
