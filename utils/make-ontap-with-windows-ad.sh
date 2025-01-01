ee164d54dc9f5929feef072ebdf1f4436e17557c
Jianhong Yin <jiyin@redhat.com>
2025-01-01 15:14:13 +0800

    make-ontap-with-windows-ad.sh: show gssd status while krb5-mnt fail
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/make-ontap-with-windows-ad.sh
+++ utils/make-ontap-with-windows-ad.sh
@@ -198,9 +198,12 @@ vm exec -vx $clientvm -- ping -c 4 $NETAPP_NAS_HOSTNAME
 vm exec -vx $clientvm -- systemctl restart nfs-client.target gssproxy.service rpc-statd.service rpc-gssd.service
 vm exec -vx $clientvm -- mkdir -p $nfsmp_krb5 $nfsmp_krb5i $nfsmp_krb5p
 vm exec -v  $clientvm -- showmount -e $NETAPP_NAS_HOSTNAME
-vm exec -vx $clientvm -- mount $NETAPP_NAS_HOSTNAME:$NETAPP_NFS_SHARE2 $nfsmp_krb5 -osec=krb5
-vm exec -vx $clientvm -- mount $NETAPP_NAS_HOSTNAME:$NETAPP_NFS_SHARE2 $nfsmp_krb5i -osec=krb5i
-vm exec -vx $clientvm -- mount $NETAPP_NAS_HOSTNAME:$NETAPP_NFS_SHARE2 $nfsmp_krb5p -osec=krb5p
+vm exec -vx $clientvm -- mount $NETAPP_NAS_HOSTNAME:$NETAPP_NFS_SHARE2 $nfsmp_krb5 -osec=krb5 && {
+	vm exec -vx $clientvm -- mount $NETAPP_NAS_HOSTNAME:$NETAPP_NFS_SHARE2 $nfsmp_krb5i -osec=krb5i
+	vm exec -vx $clientvm -- mount $NETAPP_NAS_HOSTNAME:$NETAPP_NFS_SHARE2 $nfsmp_krb5p -osec=krb5p
+} || {
+	vm exec -v  $clientvm -- systemctl status rpc-gssd
+}
 vm exec -vx $clientvm -- mount -t nfs4
 vm exec -vx $clientvm -- umount -a -t nfs4,nfs
 vm exec -vx $clientvm -- "hostname -A | grep -w $netbiosname"
