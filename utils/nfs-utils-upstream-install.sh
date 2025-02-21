68f696b3a0d894e32b49972c4a9643975cbb91c0
Jianhong Yin <jiyin@redhat.com>
2025-02-21 16:40:52 +0800

    utils/nfs-utils-upstream.sh: optimize code and add workaround for
    for missing new dependency packages
    
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/nfs-utils-upstream-install.sh
+++ utils/nfs-utils-upstream-install.sh
@@ -2,22 +2,25 @@
 
 RC=1
 gitUrl=git://git.linux-nfs.org/projects/steved/nfs-utils.git
+pkgs='git make'
 
-if ! { command -v git && command -v make; }; then
-	if command -v dnf &>/dev/null; then
-		dnf install -y git make
-	elif command -v apt &>/dev/null; then
-		apt install -o APT::Install-Suggests=0 -o APT::Install-Recommends=0 -y git make
-	elif command -v zypper &>/dev/null; then
-		zypper in --no-recommends -y git make
-	elif command -v pacman &>/dev/null; then
-		pacman -Sy --noconfirm git make
-	fi
+if command -v dnf &>/dev/null; then
+	dnf install -y $pkgs
+elif command -v apt &>/dev/null; then
+	apt install -o APT::Install-Suggests=0 -o APT::Install-Recommends=0 -y $pkgs
+elif command -v zypper &>/dev/null; then
+	zypper in --no-recommends -y $pkgs
+elif command -v pacman &>/dev/null; then
+	pacman -Sy --noconfirm $pkgs
 fi
 
+rm -rf nfs-utils
 if git clone $gitUrl; then
 	pushd nfs-utils
 		bash install-dep
+		#now (2025-02-21) the install-dep is not cover all dependency that required by configure
+		#here is a workaround on fedora/rhel os
+		dnf install -y libmount-devel libnl3-devel readline-devel libxml2-devel
 		if ./autogen.sh && ./configure && make && make install; then
 			mount.nfs -V
 			RC=0
