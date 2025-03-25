1922033ac1170305eccf322c381393e1e9bb4b9d
Jianhong Yin <jiyin@redhat.com>
2025-03-25 18:29:45 +0800

    utils/cpu-temp-and-hz.sh: fix code that get temp by using sensors
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/cpu-temp-and-hz.sh
+++ utils/cpu-temp-and-hz.sh
@@ -10,8 +10,12 @@ watch -t -n 1 '
 			echo -en "${tempf##*/thermal_}: $(($(cat $tempf/temp 2>/dev/null||echo 0)/1000))$tempUnit,"
 		done
 		echo "}"
+	elif command -v sensors &>/dev/null; then
+		sensors coretemp-isa-0000 2>/dev/null ||
+			sensors k10temp-pci-00c3 | awk '\''$2~/^+/{print "cpu-core:", $2}'\''
+			sensors amdgpu-pci-0400 | awk '\''$2~/^+/{print "amdgpu-core:", $2}'\''
 	else
-		sensors|sed -n "/Physical id 0/{s/[^+]*+//; p;}"
+		echo "{warn} There is neither '/sys/class/thermal/*/temp' file nor command 'sensors'" >&2
 	fi
 	echo
 	grep cpu.MHz /proc/cpuinfo | sort'
