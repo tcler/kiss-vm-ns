f87178563e5913307dd7890cb0cb66eba2402236
Jianhong Yin <jiyin@redhat.com>
2025-04-04 17:03:07 +0800

    kiss-vm: update default ram size from 1.5G to 2G
    and remove spaces at end of line
    
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/git-force-sync-from-upstream.sh
+++ utils/git-force-sync-from-upstream.sh
@@ -21,7 +21,7 @@ forceSyncFromUpstream() {
 
 	git fetch upstream
 	git checkout ${master}
-	git reset --hard upstream/${master}  
+	git reset --hard upstream/${master}
 	git push origin ${master} --force
 }
 
