9c1bd19f4979d006dd83a87697e76ccc8eb77fa9
Jianhong Yin <jiyin@redhat.com>
2025-05-29 11:40:04 +0800

    utils/nfstest-result-check.sh: optimize code and fix typo
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/nfstest-result-check.sh
+++ utils/nfstest-result-check.sh
@@ -4,8 +4,12 @@ nfstestFailChk() {
 	local rc=0
 	local exnfail=$1 npass= nfail=
 	local testlog=$2
+	if [[ ! -r ${testlog} ]]; then
+		echo -e "\E[1;31m{nfstest fail check} test log file(${testlog}) not accessable/exist.\E[0m"
+		return 2
+	fi
 	read _ npass nfail < <(awk -F'[ ()]+' '/^[0-9]+ tests /{print $1,$3,$5}' $testlog)
-	if [[ ${nfail} -gt ${exfail} ]]; then
+	if [[ ${nfail} -gt ${exnfail} ]]; then
 		echo -e "\E[1;31m{nfstest fail check} fail number(${nfail}) is great than expected(${exnfail})\E[0m"
 		rc=1
 	fi
