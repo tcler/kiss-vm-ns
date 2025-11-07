85f4b7d99f6848c3248db931d1d77096ede3a2fc
Jianhong Yin <jiyin@redhat.com>
2025-11-07 08:25:25 +0800

    utils/dovpn-connect.sh: fix connect fail without username
    and optimize code
    
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/dovpn-connect.sh
+++ utils/dovpn-connect.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
+# just for fun, and ease my vpn connect
 
 [[ -n "$SSH_CLIENT" || -n "$SSH_TTY" ]] && exec sudo $0 "$@"
+
+askuser() {
+	local nmc="${*}"
+	local u=$(nmcli -g vpn.user-name c s "${nmc}")
+	local iuser=
+	while [[ -z "$iuser" ]]; do
+		read -p "Enter username([$u]): " iuser
+		if [[ -n "$iuser" ]]; then
+			[[ "$iuser" != "$u" ]] &&
+				nmcli c modify "${nmc}" vpn.user-name $iuser
+		elif [[ -n "$u" ]]; then
+			iuser=$u
+		else
+			echo "{warn} vpn.user-name is empty" >&2
+		fi
+	done
+}
 
 downvpns=()
 upvpns=()
@@ -14,19 +32,26 @@ done < <(nmcli connection show | awk '
 	}')
 
 if [[ $1 != d* ]]; then
+	if [[ ${#upvpns} -ne 0 ]]; then
+		echo "{warn} there is vpn already connected" >&2
+		nmcli connection show | awk '$(NF-1) == "vpn" && $NF != "--"'
+	fi
+
 	if [[ ${#downvpns[@]} -eq 0 ]]; then
 		echo "{warn} did not find vpn connection by using: nmcli c s" >&2
 		exit 1
 	elif [[ ${#downvpns[@]} -eq 1 ]]; then
 		echo "{exec} nmcli connection --ask up '${downvpns}'"
+		askuser "${downvpns}"
 		nmcli connection --ask up "${downvpns}"
 		exit
 	fi
 
-	select vpn in /quit "${downvpns[@]}"; do
+	select vpn in "${downvpns[@]}"; do
 		[[ -z "${vpn}" || ${vpn} = /q* ]] && break
-		echo "{info} up the vpn(${vpn}) you selected ..."
+		echo "{info} up/connect the vpn(${vpn}) you selected ..."
 		echo "{exec} nmcli connection --ask up '${vpn}'"
+		askuser "${vpn}"
 		nmcli connection --ask up "${vpn}" && {
 			nmcli c s | grep -F "${vpn}"
 			break
@@ -42,13 +67,11 @@ else
 		exit
 	fi
 
-	select vpn in /quit "${upvpns[@]}"; do
+	select vpn in "${upvpns[@]}"; do
 		[[ -z "${vpn}" || ${vpn} = /q* ]] && break
-		echo "{info} up the vpn(${vpn}) you selected ..."
-		echo "{exec} nmcli connection --ask up '${vpn}'"
-		nmcli connection --ask up "${vpn}" && {
-			nmcli c s | grep -F "${vpn}"
-			break
-		}
+		echo "{info} down/disconnect the vpn(${vpn}) you selected ..."
+		echo "{exec} nmcli connection down '${vpn}'"
+		nmcli connection down "${vpn}"
+		break
 	done
 fi
