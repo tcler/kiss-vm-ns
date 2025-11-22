af0b6fbe676afe41dcb10ee4656ac83b9a303ca8
Jianhong Yin <jiyin@redhat.com>
2025-11-22 17:18:28 +0800

    utils/addbridge.sh: improve Usage info and optimize code
    and add proxy_arp setup if the $ifname is a wireless device
    
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/addbridge.sh
+++ utils/addbridge.sh
@@ -15,8 +15,28 @@ is_slave() {
 #__main__
 switchroot "$@"
 
+Usage() {
+	cat <<-USAGE
+	Usage: $0 [bridge name] [bridge-slave ifname] [-f] [-ni]
+	Comment:
+	  if [bridge-slave ifname] is omitted, use the default route's interface as the default.
+	  if [bridge name] is omitted, use use 'br0' by default
+	Options:
+	  -f force  #if the bridge-slave is also a bridge device, force the action
+	  -ni non-interactive mode, will not ask user to confirm
+	USAGE
+}
+
+force=no
+interactive=yes
 _at=()
-for arg; do [[ "$arg" = -f ]] && force=yes || _at+=("$arg"); done
+for arg; do
+	case $arg in
+	-f) force=yes;;
+	-ni) interactive=no;;
+	*) _at+=("$arg");;
+	esac
+done
 set -- "${_at[@]}"
 brname=${1}
 ifname=${2}
@@ -25,16 +45,18 @@ brname=${brname:-br0}
 [[ -z "$ifname" ]] && ifname=$(get-default-if.sh)
 [[ -z "$ifname" ]] && {
 	echo "{warn} there is no bridge-slave ifname specified, and auto detect fail." >&2
-	echo "{info} Usage: $0 [bridge name] [bridge-slave ifname]" >&2
+	Usage >&2
 	exit 1
 }
 
 if ip addr show dev $brname &>/dev/null; then
 	echo "{warn} bridge dev '$brname' has been there" >&2
+	Usage >&2
 	exit 1
 fi
 if br=$(is_slave $ifname); then
 	echo "{warn} network interface '$ifname' has been a bridge-slave of '$br'" >&2
+	Usage >&2
 	exit 1
 fi
 if is_bridge $ifname && [[ "$force" != yes ]]; then
@@ -42,9 +64,25 @@ if is_bridge $ifname && [[ "$force" != yes ]]; then
 	exit 1
 fi
 
+echo "{info} will create bridge '$brname' and add '$ifname' in it as bridge-slave."
+read -p "Add it might cause network connection break. Are you sure?(Y/N): " answer
+if [[ "$answer" != [Yy]* ]]; then
+    echo "OK, let's quit"
+    exit 0
+fi
+
 coname=$(nmcli -g GENERAL.CONNECTION device show $ifname)
 nmcli c delete "$coname" 2>/dev/null
 nmcli c add type bridge ifname $brname stp off autoconnect yes
 nmcli c add type bridge-slave ifname "$ifname" master $brname autoconnect yes
+
+if [[ -d /sys/class/net/$ifname/wireless ]]; then
+	echo "{info} enable proxy_arp, because '$ifname' is a wireless dev" >&2
+	sysctlf=/etc/sysctl.d/100-kissvm-proxy-arp.conf
+	for nic in $ifname $brname; do
+		echo "net.ipv4.conf.${nic}.proxy_arp = 1"
+	done | tee $sysctlf
+	sysctl -p $sysctlf
+fi
 
 systemctl restart NetworkManager
