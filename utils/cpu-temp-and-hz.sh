165d0c018fd1a33911960d85ca3667c06e00b1e8
Jianhong Yin <jiyin@redhat.com>
2026-02-20 18:19:20 +0800

    utils/cpu-temp-and-hz.sh: let it works on debian/arm64
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/cpu-temp-and-hz.sh
+++ utils/cpu-temp-and-hz.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-watch -t -n 1 '
-	date
+watch -t -n 1 bash -c 'date
 	echo
 	if test -f /sys/class/thermal/thermal_zone0/temp; then
-		tempUnit=$"\\U2103"; [[ "$LANG" = C ]] && tempUnit=^C
-		echo -n "CPU temp: {"
+		tempUnit=$"\\U2103";
+		test "$LANG" = C && tempUnit=^C;
+		test "$0" = sh && tempUnit=^C;
+		/bin/echo -n "CPU temp: {"
 		for tempf in /sys/class/thermal/thermal_zone*; do
-			echo -en "${tempf##*/thermal_}: $(($(cat $tempf/temp 2>/dev/null||echo 0)/1000))$tempUnit,"
+			/bin/echo -en "${tempf##*/thermal_}: $(($(cat $tempf/temp 2>/dev/null||echo 0)/1000))$tempUnit, "
 		done
 		echo "}"
 	elif command -v sensors &>/dev/null; then
@@ -15,7 +16,8 @@ watch -t -n 1 '
 			sensors -A | awk -v RS= '\''/k10temp/ {if (match($0, /\+[^ ]+/,m)) {print "cpu:", m[0]}}'\''
 			sensors -A | awk -v RS= '\''/amdgpu/{if (match($0, /\+[^ ]+/,m)) {print "amdgpu:", m[0]}}'\''
 	else
-		echo "{warn} There is neither '/sys/class/thermal/*/temp' file nor command 'sensors'" >&2
+		/bin/echo "{warn} There is neither '/sys/class/thermal/*/temp' file nor command 'sensors'" >&2
 	fi
 	echo
-	grep cpu.MHz /proc/cpuinfo | sort'
+	bash -c "grep cpu.MHz /proc/cpuinfo > >(sort)" ||
+	cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_cur_freq'
